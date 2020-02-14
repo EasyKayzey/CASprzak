20d4b6d3dff1a0796aa2a67afe02825d846b9dab
michaelk522 <michaelkasprzak522@gmail.com>
2020-02-14 08:56:12 -0500

    Fix the tests

--- tests/DerivativeTest.java
+++ tests/DerivativeTest.java
@@ -1,20 +1,24 @@
-//import CASprzak.OldFunction;
-//import org.junit.jupiter.api.Test;
-//
-//import static org.junit.jupiter.api.Assertions.assertEquals;
-//import static org.junit.jupiter.api.Assertions.assertSame;
-//
-//public class DerivativeTest {
-//    @Test void constantsGive0() {
-//        OldFunction test = OldFunction.makeFunction("2");
-//        assertEquals(0, test.derivative("x").evaluate(new String[] {"x"}, new double[]{3467}));
-//    }
-//
-//    @Test void variablesGive1() {
-//        OldFunction test = OldFunction.makeFunction("x");
-//        assertEquals(1, test.derivative("x").evaluate(new String[] {"x"}, new double[]{3467}));
-//    }
-//
-//
-//
-//}
+
+import CASprzak.Function;
+import CASprzak.Parser;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertSame;
+
+public class DerivativeTest {
+    @Test void constantsGive0() throws Exception {
+        Parser parser = new Parser(new char[]{'x'});
+        Function test = parser.parse("2");
+        assertEquals(0, test.getDerivative(0).evaluate(new double[]{3467}));
+    }
+
+    @Test void variablesGive1() throws Exception {
+        Parser parser = new Parser(new char[]{'x'});
+        Function test = parser.parse("x");
+        assertEquals(1, test.getDerivative(0).evaluate(new double[]{3467}));
+    }
+
+
+
+}
--- tests/OldFunctionTest.java
+++ tests/OldFunctionTest.java
@@ -1,39 +1,45 @@
-//import CASprzak.OldFunction;
-//import org.junit.jupiter.api.Test;
-//
-//import static org.junit.jupiter.api.Assertions.assertEquals;
-//import static org.junit.jupiter.api.Assertions.assertSame;
-//
-//public class OldFunctionTest {
-//	@Test void fxReturnsX() {
-//		OldFunction test = OldFunction.makeFunction("x");
-//		double b = test.evaluate(new String[]{"x"}, new double[]{2});
-//		assertEquals(2, b);
-//	}
-//
-//	@Test void basicFunctionsWithX() {
-//		OldFunction test = OldFunction.makeFunction("x ^ x + 2 + 5 * x");
-//		double b = test.evaluate(new String[]{"x"}, new double[]{3});
-//		assertEquals(44, b);
-//	}
-//
-//	@Test void multipleVariablesWorks() {
-//		OldFunction test = OldFunction.makeFunction("x + y + y");
-//		double b = test.evaluate(new String[]{"x", "y"}, new double[]{3, 4});
-//		assertEquals(11, b);
-//	}
-//
-//	@Test void logbWorks() {
-//		OldFunction test = OldFunction.makeFunction("logb 3 x");
-//		double b = test.evaluate(new String[]{"x"}, new double[]{81});
-//		assertEquals(4, b);
-//	}
-//
-//	@Test void trigWorks() {
-//		OldFunction test = OldFunction.makeFunction("( CASprzak.CASprzak.UnitaryFunctions.CASprzak.UnitaryFunctions.sin x ) ^ 2 + ( CASprzak.CASprzak.UnitaryFunctions.CASprzak.UnitaryFunctions.cos x ) ^ 2");
-//		double b = test.evaluate(new String[]{"x"}, new double[]{81});
-//		assertEquals(1, b);
-//	}
-//
-//
-//}
+import CASprzak.Function;
+import CASprzak.Parser;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertSame;
+
+public class OldFunctionTest {
+	@Test void fxReturnsX() throws Exception {
+	    Parser parser = new Parser(new char[]{'x'});
+		Function test = parser.parse("x");
+		double b = test.evaluate(new double[]{2});
+		assertEquals(2, b);
+	}
+
+	@Test void basicFunctionsWithX() throws Exception {
+        Parser parser = new Parser(new char[]{'x'});
+        Function test = parser.parse("x ^ x + 2 + 5 * x");
+		double b = test.evaluate(new double[]{3});
+		assertEquals(44, b);
+	}
+
+	@Test void multipleVariablesWorks() throws Exception {
+        Parser parser = new Parser(new char[]{'x', 'y'});
+        Function test = parser.parse("x + y + y");
+		double b = test.evaluate(new double[]{3, 4});
+		assertEquals(11, b);
+	}
+
+	@Test void logbWorks() throws Exception {
+        Parser parser = new Parser(new char[]{'x'});
+        Function test = parser.parse("logb 3 x");
+		double b = test.evaluate(new double[]{81});
+		assertEquals(4, b);
+	}
+
+	@Test void trigWorks() throws Exception {
+        Parser parser = new Parser(new char[]{'x'});
+        Function test = parser.parse("( sin x ) ^ 2 + ( cos x ) ^ 2");
+		double b = test.evaluate(new double[]{81});
+		assertEquals(1, b);
+	}
+
+
+}

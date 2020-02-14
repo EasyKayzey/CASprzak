a3a6ae1c46aea925b5c3b8b104464a858b2f77fe
Erez Israeli Miller <erez.m.israeli@gmail.com>
2020-02-13 19:31:57 -0500

    Changed test name

--- tests/FunctionTest.java
+++ tests/FunctionTest.java
@@ -2,7 +2,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertSame;
 
 public class FunctionTest {
-	@Test void testOneIsGood() {
+	@Test void fxReturnsX() {
 
 	}
 }

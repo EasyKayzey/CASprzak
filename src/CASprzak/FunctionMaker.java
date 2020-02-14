b0219d82edf0e5ca32e1a815c00fe706f7926b4d
Erez Israeli Miller <erez.m.israeli@gmail.com>
2020-02-13 23:54:37 -0500

    Import packages to FunctionMaker

--- src/CASprzak/FunctionMaker.java
+++ src/CASprzak/FunctionMaker.java
@@ -1,5 +1,10 @@
 package CASprzak;
 
+import CASprzak.UnitaryFunctions.*;
+import CASprzak.BinaryFunctions.*;
+import CASprzak.CommutativeFunctions.*;
+import CASprzak.SpecialFunctions.*;
+
 public class FunctionMaker {
 	public Function constant(double parseDouble) {
 	}
@@ -8,6 +13,9 @@ public class FunctionMaker {
 	}
 
 	public Function find1(String i, Function c) {
+		switch (i) {
+			case "sin": return new Sin(c);
+		}
 	}
 
 	public Function find2(String i, Function a, Function b) {

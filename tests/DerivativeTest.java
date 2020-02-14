f2c1a3e2c041187762de205a9da79ed95c8b7b77
michaelk522 <michaelkasprzak522@gmail.com>
2020-02-13 19:53:44 -0500

    Make tests

--- tests/DerivativeTest.java
+++ tests/DerivativeTest.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertSame;
 
 public class DerivativeTest {
+    @Test void constantsGive0() {
+        Function test = Function.makeFunction("2");
+        assertEquals(0, test.derivative("x").evaluate(new String[] {"x"}, new double[]{3467}));
+    }
+
+    @Test void variablesGive1() {
+        Function test = Function.makeFunction("x");
+        assertEquals(1, test.derivative("x").evaluate(new String[] {"x"}, new double[]{3467}));
+    }
+
+
+
 }
--- tests/FunctionTest.java
+++ tests/FunctionTest.java
@@ -1,8 +1,26 @@
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertSame;
 
 public class FunctionTest {
 	@Test void fxReturnsX() {
-
+		Function test = Function.makeFunction("x");
+		double b = test.evaluate(new String[]{"x"}, new double[]{2});
+		assertEquals(2, b);
 	}
+
+	@Test void basicFunctionsWithX() {
+		Function test = Function.makeFunction("x ^ x + 2 + 5 * x");
+		double b = test.evaluate(new String[]{"x"}, new double[]{3});
+		assertEquals(44, b);
+	}
+
+	@Test void multipleVariablesWorks() {
+		Function test = Function.makeFunction("x + y + y");
+		double b = test.evaluate(new String[]{"x", "y"}, new double[]{3, 4});
+		assertEquals(11, b);
+	}
+
+
 }

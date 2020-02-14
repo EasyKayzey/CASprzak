b261cbcc1c1c0c79c363f5c42b5dbe4d4ccd9a90
Erez Israeli Miller <erez.m.israeli@gmail.com>
2020-02-14 07:57:29 -0500

    Fix variable generation

--- src/CASprzak/Parser.java
+++ src/CASprzak/Parser.java
@@ -6,6 +6,8 @@ public class Parser {
   public static final String[] operations2 = {"^", "*", "/", "+", "-", "logb"};
   public static final String[] operations1 = {"sin", "cos", "tan", "log", "ln", "sqrt", "exp", "sinh", "cosh", "tanh", "csc", "sec", "cot", "asin", "acos", "atan"};
 
+  private char[] variables = {};
+
   public boolean isAnOperator1(String input) {
     for (String x : operations1) {
       if(x.equals(input)) return true;
@@ -20,24 +22,34 @@ public class Parser {
     return false;
   }
 
+  public void setVariables(char[] variables) {
+    this.variables = variables;
+  }
+
+  public int getVarID(char variable) {
+    for (int i = 0; i < variables.length; i++) if (variables[i] == v) {
+      return i;
+    }
+  }
+
   public Function parse(String[] postfix) throws Exception {
 
     FunctionMaker functionMaker = new FunctionMaker();
     Stack<Function> functionStack = new Stack<Function>();
-    for (String i : postfix) {
-      if (!isAnOperator1(i) && !isAnOperator2(i)) {
+    outer: for (String token : postfix) {
+      if (!isAnOperator1(token) && !isAnOperator2(token)) {
         try {
-          functionStack.push(functionMaker.constant(Double.parseDouble(i)));
+          functionStack.push(functionMaker.constant(Double.parseDouble(token)));
         } catch (Exception e) {
-          if (i.length() > 1) System.out.println(i + " is not a valid function.");
-          functionStack.push(functionMaker.variable(0, new char[]{'x'}));
-
+          if (token.length() > 1) System.out.println(token + " is not a valid function.");
+          char v = token.charAt(0);
+          functionStack.push(functionMaker.variable(getVarID(v), new char[]{v}));
         }
-      } else if (isAnOperator2(i)) {
+      } else if (isAnOperator2(token)) {
         Function a = functionStack.pop();
         Function b = functionStack.pop();
         functionStack.push(functionMaker.find2(i, a, b));
-      } else if (isAnOperator1(i)) {
+      } else if (isAnOperator1(token)) {
         Function c = functionStack.pop();
         functionStack.push(functionMaker.find1(i, c));
       }

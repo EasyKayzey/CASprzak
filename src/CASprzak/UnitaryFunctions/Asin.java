062952d46c20c27f20a2302ea1f27b21cf22dcb5
michaelk522 <michaelkasprzak522@gmail.com>
2020-02-13 22:44:23 -0500

    Fix lowercase Function names

---
+++ src/CASprzak/UnitaryFunctions/Asin.java
@@ -0,0 +1,13 @@
+package CASprzak.UnitaryFunctions;
+
+public class Asin extends UnitaryFunction {
+    @Override
+    public String toString() {
+        return null;
+    }
+
+    @Override
+    public double evaluate(double[] variableValues) {
+        return 0;
+    }
+}
---
+++ src/CASprzak/UnitaryFunctions/Csc.java
@@ -0,0 +1,13 @@
+package CASprzak.UnitaryFunctions;
+
+public class Csc extends UnitaryFunction {
+    @Override
+    public String toString() {
+        return null;
+    }
+
+    @Override
+    public double evaluate(double[] variableValues) {
+        return 0;
+    }
+}

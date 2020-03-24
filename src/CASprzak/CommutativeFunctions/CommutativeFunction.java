96bb70301c9407323abb6b4eb9807b945ff34ad9
eiis1000 <erez.m.israeli@gmail.com>
2020-03-24 13:24:03 -0400

    Make ...-based constructor in CommutativeFunction

--- src/CASprzak/CommutativeFunctions/CommutativeFunction.java
+++ src/CASprzak/CommutativeFunctions/CommutativeFunction.java
@@ -8,14 +8,9 @@ public abstract class CommutativeFunction extends Function {
 
     protected final Function[] functions;
 
-    public CommutativeFunction(Function[] functions) {
+    public CommutativeFunction(Function... functions) {
         this.functions = functions;
     }
-
-    public CommutativeFunction(Function f1, Function f2) {
-        this.functions = new Function[] {f1, f2};
-    }
-
 
     public CommutativeFunction simplifyInternal() {
         return this.simplifyElements().simplifyIdentity().simplifyConstants();

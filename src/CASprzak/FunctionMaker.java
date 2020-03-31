4818eb728bb09a2b55bd794026d2e1717ae98326
eiis1000 <erez.m.israeli@gmail.com>
2020-03-31 12:57:01 -0400

    Refactor FunctionMaker

--- src/CASprzak/FunctionMaker.java
+++ src/CASprzak/FunctionMaker.java
@@ -17,43 +17,36 @@ public class FunctionMaker {
 		return new Variable(varID, varNames);
 	}
 
-	public Function find1(String i, Function c) {
-		switch (i) {
-			case "sin": return new Sin(c);
-			case "cos": return new Cos(c);
-			case "tan": return new Tan(c);
-			case "csc": return new Csc(c);
-			case "sec": return new Sec(c);
-			case "cot": return new Cot(c);
-			case "asin": return new Asin(c);
-			case "acos": return new Acos(c);
-			case "atan": return new Atan(c);
-			case "sinh": return new Sinh(c);
-			case "cosh": return new Cosh(c);
-			case "tanh": return new Tanh(c);
-			case "ln": return new Ln(c);
-			case "negative": return new Negative(c);
-			case "reciprocal": return new Reciprocal(c);
-
+	public Function find1(String functionName, Function function) {
+		switch (functionName) {
+			case "sin": return new Sin(function);
+			case "cos": return new Cos(function);
+			case "tan": return new Tan(function);
+			case "csc": return new Csc(function);
+			case "sec": return new Sec(function);
+			case "cot": return new Cot(function);
+			case "asin": return new Asin(function);
+			case "acos": return new Acos(function);
+			case "atan": return new Atan(function);
+			case "sinh": return new Sinh(function);
+			case "cosh": return new Cosh(function);
+			case "tanh": return new Tanh(function);
+			case "ln": return new Ln(function);
+			case "negative": return new Negative(function);
+			case "reciprocal": return new Reciprocal(function);
+			default: throw new IllegalArgumentException("Invalid functionName " + functionName);
 		}
-		return c;
 	}
 
-	public Function find2(String i, Function a, Function b) throws Exception {
-		switch (i) {
-			case "+":
-				return new Add(a, b);
-			case "-":
-				return new Add(b, new Negative(a));
-			case "*":
-				return new Multiply(a, b);
-			case "/":
-				return new Multiply(b, new Reciprocal(a));
-			case "^":
-				return new Pow(a, b);
-			case "logb":
-				return new Logb(a, b);
+	public Function find2(String functionName, Function function1, Function function2) {
+		switch (functionName) {
+			case "+": return new Add(function1, function2);
+			case "-": return new Add(function2, new Negative(function1));
+			case "*": return new Multiply(function1, function2);
+			case "/": return new Multiply(function2, new Reciprocal(function1));
+			case "^": return new Pow(function1, function2);
+			case "logb": return new Logb(function1, function2);
+			default: throw new IllegalArgumentException("Invalid functionName " + functionName);
 		}
-		throw new Exception("Function " + i + " does not exist");
 	}
 }

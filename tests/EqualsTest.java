52922348e49ab30c4a6534f876f7540bec0548d9
eiis1000 <erez.m.israeli@gmail.com>
2020-03-31 13:06:02 -0400

    Rename EqualsName.java to EqualsTest.java

--- tests/EqualsTest.java
+++ tests/EqualsTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class EqualsName {
+public class EqualsTest {
 
         @Test
         void unitaryEqualsUnitary() throws Exception {

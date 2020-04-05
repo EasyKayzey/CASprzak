a88fa9133c7def64347d03bbcb22f38342ddd333
michaelk522 <michaelkasprzak522@gmail.com>
2020-04-05 18:41:36 -0400

    Redo log base

--- src/CASprzak/BinaryFunctions/Logb.java
+++ src/CASprzak/BinaryFunctions/Logb.java
@@ -1,7 +1,10 @@
 package CASprzak.BinaryFunctions;
 
+import CASprzak.CommutativeFunctions.Add;
+import CASprzak.CommutativeFunctions.Multiply;
 import CASprzak.Function;
 import CASprzak.SpecialFunctions.Constant;
+import CASprzak.UnitaryFunctions.Ln;
 
 public class Logb extends BinaryFunction {
     public Logb(Function function1, Function function2) {
@@ -15,9 +18,7 @@ public class Logb extends BinaryFunction {
 
     @Override
     public Function getDerivative(int varID) {
-        //TODO
-        System.out.println("PLS IMPLEMENT");
-        return new Constant(0);
+        return new Multiply(new Add(new Multiply(function1.getSimplifiedDerivative(varID), new Ln(function2), new Pow(new Constant(-1), function1)), new Multiply(new Constant(-1), function2.getSimplifiedDerivative(varID), new Ln(function1), new Pow(new Constant(-1), function2))), new Pow(new Constant(-2), new Ln(function2)));
     }
 
     @Override

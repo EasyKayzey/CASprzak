a664aee66c83c485af221fa7a643112e361aa413
michaelk522 <michaelkasprzak522@gmail.com>
2020-04-05 19:50:21 -0400

    Add tests in functions

--- tests/FunctionTest.java
+++ tests/FunctionTest.java
@@ -10,33 +10,64 @@ public class FunctionTest {
 
 	@Test void fxReturnsX() {
 		Function test = parserX.parse("x");
-		double b = test.evaluate(new double[]{2});
+		double b = test.evaluate(2);
 		assertEquals(2, b);
 	}
 
 	@Test void basicFunctionsWithX() {
 		Function test = parserX.parse("x ^ x + 2 + 5 * x");
-		double b = test.evaluate(new double[]{3});
+		double b = test.evaluate(3);
 		assertEquals(44, b);
 	}
 
 	@Test void multipleVariablesWorks() {
         Function test = parserXY.parse("x + y + y");
-		double b = test.evaluate(new double[]{3, 4});
+		double b = test.evaluate(3, 4);
 		assertEquals(11, b);
 	}
 
 	@Test void logbWorks() {
 		Function test = parserX.parse("logb 3 x");
-		double b = test.evaluate(new double[]{81});
+		double b = test.evaluate(81);
 		assertEquals(4, b);
 	}
 
 	@Test void trigWorks() {
 		Function test = parserX.parse("( sin x ) ^ 2 + ( cos x ) ^ 2");
-		double b = test.evaluate(new double[]{81});
+		double b = test.evaluate(81);
 		assertEquals(1, b);
 	}
+
+	@Test void eToTheXWorks() {
+		Function test = parserX.parse("e ^ x");
+		double b = test.evaluate(0);
+		assertEquals(1, b);
+	}
+
+	@Test void lnWorks() {
+		Function test = parserX.parse("ln e");
+		double b = test.evaluate(0);
+		assertEquals(1, b);
+	}
+
+	@Test void tanWorks() {
+		Function test = parserX.parse("tan x");
+		double b = test.evaluate(Math.PI/4);
+		assertEquals(1, b);
+	}
+
+	@Test void multiplyWorks() {
+		Function test = parserX.parse("( 1 / ( x + 1 ) ) * ( cos x ) * ( x ^ 2 - 1 )");
+		double b = test.evaluate(0);
+		assertEquals(-1, b);
+	}
+
+	@Test void addWorks() {
+		Function test = parserX.parse("( 1 / ( x + 1 ) ) + ( sec x ) + ( x ^ 3 - 1 )");
+		double b = test.evaluate(0);
+		assertEquals(1, b);
+	}
+
 
 
 }

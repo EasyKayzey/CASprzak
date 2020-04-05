fa20d54bd843c0b24912ba033d57738d314ba16b
michaelk522 <michaelkasprzak522@gmail.com>
2020-04-04 23:33:38 -0400

    Finish addExponents

--- src/CASprzak/ArrLib.java
+++ src/CASprzak/ArrLib.java
@@ -2,44 +2,44 @@ package CASprzak;
 
 public class ArrLib {
 	public static Function[] removeFunctionAt(Function[] functionArray, int index) {
-		Function[] newArray = new Function[functionArray.length-1];
-		for (int i = 0; i < newArray.length; i++)
-				newArray[i] = functionArray[(i<index?i:i+1)];
-		return newArray;
+		Function[] fout = new Function[functionArray.length-1];
+		for (int i = 0; i < fout.length; i++)
+				fout[i] = functionArray[(i<index?i:i+1)];
+		return fout;
 	}
 
 	public static Function[] deepClone(Function[] functionArray) {
-		Function[] newArray = new Function[functionArray.length];
-		for (int i = 0; i < newArray.length; i++)
-			newArray[i] = functionArray[i].clone();
-		return newArray;
+		Function[] fout = new Function[functionArray.length];
+		for (int i = 0; i < fout.length; i++)
+			fout[i] = functionArray[i].clone();
+		return fout;
 	}
 
 	public static boolean deepEquals(Function[] functionArray1, Function[] functionArray2) {
 		if (functionArray1.length != functionArray2.length)
 			return false;
 		for (int i = 0; i < functionArray1.length; i++) {
-			if (!functionArray1[i].equals(functionArray2[i])) {
+			if (!functionArray1[i].equals(functionArray2[i]))
 				return false;
-			}
 		}
 		return true;
 	}
 
 	public static Function[] pullUp(Function[] outer, Function[] inner, int indexInFirst) {
-		Function[] newArray = new Function[outer.length + inner.length - 1];
-		if (indexInFirst >= 0)
-			System.arraycopy(outer, 0, newArray, 0, indexInFirst);
-		if (outer.length - indexInFirst + 1 >= 0)
-			System.arraycopy(outer, indexInFirst + 1, newArray, indexInFirst, outer.length - indexInFirst + 1);
-		System.arraycopy(inner, 0, newArray, outer.length - 1, inner.length);
-		return newArray;
+		Function[] an = new Function[outer.length + inner.length - 1];
+		for (int i = 0; i < indexInFirst; i++)
+			an[i] = outer[i];
+		for (int i = indexInFirst + 1; i < outer.length; i++)
+			an[i - 1] = outer[i];
+		for (int i = 0; i < inner.length; i++)
+			an[outer.length + i - 1] = inner[i];
+		return an;
 	}
 
 	public static double[] createRange(double upper, double lower, int sections) {
 		double[] range = new double[sections+1];
 		for (int i = 0; i < range.length; i++)
-			range[i] = lower + i * (upper-lower) / sections;
+			range[i] = lower + i * (upper - lower) / sections;
 		return range;
 	}
 

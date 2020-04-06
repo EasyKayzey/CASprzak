7f336dd4127c4e87fb4433c0bc9192e07862c29a
eiis1000 <erez.m.israeli@gmail.com>
2020-04-05 20:44:33 -0400

    Add EqualsTest.constantEquality

--- tests/EqualsTest.java
+++ tests/EqualsTest.java
@@ -48,4 +48,18 @@ public class EqualsTest {
         Function test2 = parser.parse("x + 2");
         assertEquals(test1, test2);
     }
+
+    @Test
+    void constantEquality() {
+        Function test1, test2;
+        test1 = parser.parse("1");
+        test2 = parser.parse("0 + 1");
+        assertEquals(test1, test2);
+        test1 = parser.parse("1");
+        test2 = parser.parse("2");
+        assertNotEquals(test1, test2);
+        test1 = parser.parse("e");
+        test2 = parser.parse("" + Math.E);
+        assertEquals(test1, test2);
+    }
 }

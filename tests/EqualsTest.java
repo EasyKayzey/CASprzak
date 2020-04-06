bf7517f16ed4827ae7fa60643b78b1636d42edce
eiis1000 <erez.m.israeli@gmail.com>
2020-04-05 20:03:08 -0400

    Add EqualsTest.equalWhenSimplified

--- tests/EqualsTest.java
+++ tests/EqualsTest.java
@@ -9,37 +9,43 @@ public class EqualsTest {
 
     @Test
     void unitaryEqualsUnitary() {
-        Function test1= parser.parse("sin ( x )");
+        Function test1 = parser.parse("sin ( x )");
         Function test2 = parser.parse("sin ( x )");
         assertEquals(test1, test2);
     }
 
     @Test
     void variablesAreDifferent() {
-        Function test1= parser.parse("sin ( x )");
+        Function test1 = parser.parse("sin ( x )");
         Function test2 = parser.parse("sin ( y )");
         assertNotEquals(test1, test2);
     }
 
     @Test
     void differentUnitaryFunctions() {
-        Function test1= parser.parse("sin ( x )");
+        Function test1 = parser.parse("sin ( x )");
         Function test2 = parser.parse("cos ( x )");
         assertNotEquals(test1, test2);
     }
 
     @Test
     void differentFunctionTypes() {
-        Function test1= parser.parse("sin ( x )");
+        Function test1 = parser.parse("sin ( x )");
         Function test2 = parser.parse("x + 2");
         assertNotEquals(test1, test2);
     }
 
     @Test
     void multiplyAndAdd() {
-        Function test1= parser.parse("x * 2");
+        Function test1 = parser.parse("x * 2");
         Function test2 = parser.parse("x + 2");
         assertNotEquals(test1, test2);
     }
 
+    @Test
+    void equalWhenSimplified() {
+        Function test1 = parser.parse("x + (1 + 3 - 2) * 1");
+        Function test2 = parser.parse("x + 2");
+        assertEquals(test1, test2);
+    }
 }

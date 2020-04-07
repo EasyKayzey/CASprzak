c0a5195c92c95fb4a48b6d01b4e9fdcfe3c2c9d2
eiis1000 <erez.m.israeli@gmail.com>
2020-04-07 11:09:24 -0400

    Remove almost all spaces from tests to verify parser stability (some spaces left for verification of that feature)

--- src/CASprzak/PreProcessor.java
+++ src/CASprzak/PreProcessor.java
@@ -38,7 +38,7 @@ public class PreProcessor {
 	}
 
 	public String[] toPostfix(String infix) {
-		infix = infix.replaceAll("(?<![\\^\\-+*/ ])\\s*-","+-");
+		infix = infix.replace("{","(").replace("}",")").replace("\\","").replace("_"," ").replaceAll("(?<![\\^\\-+*/ ])\\s*-","+-");
 		String[] tokens = infix.split("\\s+|(((?<=\\W)(?=[\\w-])((?<!-)|(?!\\d))|(?<=\\w)(?=\\W))|(?<=[()])|(?=[()]))(?<![ .])(?![ .])");
 		System.out.println(Arrays.toString(tokens));
 		ArrayList<String> postfix = new ArrayList<>();
--- tests/FunctionTest.java
+++ tests/FunctionTest.java
@@ -14,47 +14,47 @@ public class FunctionTest {
 	}
 
 	@Test void basicFunctionsWithX() {
-		Function test = parserX.parse("x ^ x + 2 + 5 * x");
+		Function test = parserX.parse("x^x+2+5*x");
 		assertEquals(44, test.evaluate(3));
 	}
 
 	@Test void multipleVariablesWorks() {
-        Function test = parserXY.parse("x + y + y");
+        Function test = parserXY.parse("x+y+y");
 		assertEquals(11, test.evaluate(3, 4));
 	}
 
 	@Test void logbWorks() {
-		Function test = parserX.parse("logb 3 x");
+		Function test = parserX.parse("logb_3(x)");
 		assertEquals(4, test.evaluate(81));
 	}
 
 	@Test void trigWorks() {
-		Function test = parserX.parse("( sin x ) ^ 2 + ( cos x ) ^ 2");
+		Function test = parserX.parse("(sin(x))^2+(cos x)^2");
 		assertEquals(1, test.evaluate(81));
 	}
 
 	@Test void eToTheXWorks() {
-		Function test = parserX.parse("e ^ x");
+		Function test = parserX.parse("e^x");
 		assertEquals(1, test.evaluate(0));
 	}
 
 	@Test void lnWorks() {
-		Function test = parserX.parse("ln e");
+		Function test = parserX.parse("ln(e)");
 		assertEquals(1, test.evaluate(0));
 	}
 
 	@Test void tanWorks() {
-		Function test = parserX.parse("tan x");
+		Function test = parserX.parse("tan(x)");
 		assertEquals(1, test.evaluate(Math.PI/4), 0.0000001);
 	}
 
 	@Test void multiplyWorks() {
-		Function test = parserX.parse("( 1 / ( x + 1 ) ) * ( cos x ) * ( x ^ 2 - 1 )");
+		Function test = parserX.parse("(1/(x+1))*(cos x)*(x^2-1)");
 		assertEquals(-1, test.evaluate(0));
 	}
 
 	@Test void addWorks() {
-		Function test = parserX.parse("( 1 / ( x + 1 ) ) + ( sec x ) + ( x ^ 3 - 1 )");
+		Function test = parserX.parse("(1/(x+1))+(sec(x))+(x^3-1)");
 		assertEquals(1, test.evaluate(0));
 	}
 

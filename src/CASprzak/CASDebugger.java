c7b4f4de7db74e837d964c5a62fb6bcb08d2b11e
eiis1000 <erez.m.israeli@gmail.com>
2020-04-10 11:53:44 -0400

    Add evaluator to CASDebugger

--- src/CASprzak/CASDebugger.java
+++ src/CASprzak/CASDebugger.java
@@ -31,9 +31,19 @@ public class CASDebugger {
 				System.out.println("toString:");
 				System.out.println(function);
 				System.out.println("Simplified once:");
-				System.out.println(function.simplify());
+				System.out.println(function = function.simplify());
 				System.out.println("Simplified fully:");
-				System.out.println(function.simplifyTimes(10));
+				System.out.println(function = function.simplifyTimes(10));
+
+				System.out.println("Evaluate? Enter comma-separated input to evaluate or '!' otherwise.");
+				userInput = in.nextLine();
+				if (userInput.charAt(0) !=  '!') {
+					String[] unparsedValues = userInput.split(",");
+					double[] values = new double[unparsedValues.length];
+					for (int i = 0; i < values.length; i++)
+						values[i] = ConstantEvaluator.getConstant(unparsedValues[i]);
+					System.out.println(function.evaluate(values));
+				}
 				break;
 			default:
 				throw new IllegalArgumentException(userInput + " is not supported.");

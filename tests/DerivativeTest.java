84bbbcbf40f35aab15a7903d372ddf5e5fd6c555
eiis1000 <erez.m.israeli@gmail.com>
2020-04-10 13:20:12 -0400

    Finish longDerivative test

--- tests/DerivativeTest.java
+++ tests/DerivativeTest.java
@@ -35,8 +35,12 @@ public class DerivativeTest {
 
     @Test
     void longDerivative() {
-        Function test = parser.parse("(sin(x^0.5+1) * e^(x^0.5)) * x^-0.5");
-        assertEquals(-0.13874, test.getSimplifiedDerivative(0).getSimplifiedDerivative(0).evaluate(4), 0.0001);
+        Function test = parser.parse("(sin(x^0.5+1) * e^(x^0.5)) * x^-0.5").getSimplifiedDerivative(0).getSimplifiedDerivative(0);
+        assertEquals(-0.13874, test.evaluate(4), 0.0001);
+        System.out.println("Second derivative simplified once:");
+        System.out.println(test);
+        System.out.println("Second derivative simplified twice:");
+        System.out.println(test.simplify());
     }
 
 }

224308958ab3e69ea4a2b457b710c44cbec56081
eiis1000 <erez.m.israeli@gmail.com>
2020-04-10 00:19:11 -0400

    Somewhat fix regex for adjacents

--- src/CASprzak/PreProcessor.java
+++ src/CASprzak/PreProcessor.java
@@ -23,7 +23,7 @@ public class PreProcessor {
 
 	public String[] toPostfix(String infix) {
 		infix = infix.replace("{","(").replace("}",")").replace("\\","").replace("_"," ");
-		infix = infix.replaceAll("(?<=\\d)\\s*(?=[a-zA-Z])", " * ");
+		infix = infix.replaceAll("(?<=\\d)(?=[a-zA-Z])|(?<=[a-zA-Z])(?=[\\d])|(?<=\\))(?=[\\w\\(])|(?<=[\\d\\)])(?=\\()(?<!logb_)", " * ");
 		infix = infix.replaceAll("(?<!^)(?<![\\^\\-+*/ ])\\s*-","+-").replace("/","*/");
 		infix = "((((" + infix.replaceAll("\\(","((((").replaceAll("\\)","))))").replaceAll("\\+","))+((").replaceAll("\\*",")*(")+ "))))";
 		String[] tokens = infix.split("\\s+|(((?<=\\W)(?=[\\w-])((?<!-)|(?!\\d))|(?<=\\w)(?=\\W))|(?<=[()])|(?=[()]))(?<![ .])(?![ .])");

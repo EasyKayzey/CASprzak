94399a96b6be4b32f354ddc846b59f584f119b74
eiis1000 <erez.m.israeli@gmail.com>
2020-04-13 21:27:00 -0400

    Write Evaluable documentation

--- src/CASprzak/Evaluable.java
+++ src/CASprzak/Evaluable.java
@@ -1,5 +1,11 @@
 package CASprzak;
 
 interface Evaluable {
+
+	/**
+	 * Evaluates a {@link Function} at a point
+	 * @param variableValues the values of the variables of the {@link Function} at the point
+	 * @return the value of the {@link Function} at the point
+	 */
 	double evaluate(double... variableValues);
 }

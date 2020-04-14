52866b8d3122306a70aeac7149073552c62062fe
eiis1000 <erez.m.israeli@gmail.com>
2020-04-14 17:34:52 -0400

    Make InfixTokenizer

--- src/core/PreProcessor.java
+++ src/core/PreProcessor.java
@@ -44,7 +44,7 @@ public class PreProcessor {
 	 * @return array of postfix tokens
 	 */
 	public static String[] toPostfix(String infix) {
-		String[] tokens = tokenizeInfix(infix);
+		String[] tokens = InfixTokenizer.tokenizeInfix(infix, variables);
 		ArrayList<String> postfix = new ArrayList<>();
 		Stack<String> operators = new Stack<>();
 
@@ -72,41 +72,4 @@ public class PreProcessor {
 		return postfix.toArray(new String[0]);
 	}
 
-	/**
-	 * Tokenizes an input infix string into a format supported by the {@link PreProcessor}
-	 * @param infix input string in infix
-	 * @return array of infix tokens
-	 */
-	public static String[] tokenizeInfix(String infix) {
-		// Remove LaTeX escapes
-		infix = infix.replace("\\","");
-		// Make absolute values into unitary functions
-		infix = infix.replaceAll("\\|(?=([^|]*\\|[^|]*\\|)*$)",")").replaceAll("(?<=\\w)\\|","*abs(").replace("|"," abs(");
-		// Insert multiplication in expressions like 2x and 7(x*y+1)sin(3y)
-		infix = infix.replaceAll("(?<=\\d)(?=[a-zA-Z])|(?<=[a-zA-Z])(?=[\\d])|(?<=\\))(?=[\\w(])|(?<=[\\d)])(?=\\()(?<!logb_\\d)", " * ");
-		// Replace curly braces and underscores with parentheses and spaces
-		infix = infix.replace("{","(").replace("}",")").replace("_"," ");
-		// Turns expressions like x-y into x+-y, and turns expressions like x*y into x*/y (the '/' operator represents reciprocals)
-		infix = infix.replaceAll("(?<!^)(?<![\\^\\-+*/ ])\\s*-","+-").replace("/","*/");
-		// Turns expressions like xyz into x*y*z
-		infix = parseVariablePairs(infix);
-		// Adds parentheses to enforce order of operations
-		infix = "((((" + infix.replaceAll("\\(","((((").replaceAll("\\)","))))").replaceAll("\\+","))+((").replaceAll("\\*",")*(")+ "))))";
-		// Splits infix into tokens
-		return infix.split("\\s+|(((?<=\\W)(?=[\\w-])((?<!-)|(?!\\d))|(?<=\\w)(?=\\W))|(?<=[()])|(?=[()]))(?<![ .])(?![ .])");
-	}
-
-	/**
-	 * Turns pairs of variables like xy into x*y
-	 * @param infix input string in infix
-	 * @return infix string with inserted asterisks
-	 */
-	private static String parseVariablePairs(String infix) {
-		for (char a : variables) {
-			for (char b : variables) {
-				infix = infix.replaceAll("(?<="+a+")\\s*(?="+b+")","*");
-			}
-		}
-		return infix;
-	}
 }

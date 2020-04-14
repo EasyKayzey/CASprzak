58f82d6740d49e3e40f42f499d60a9771207fd42
michaelk522 <michaelkasprzak522@gmail.com>
2020-04-14 14:15:23 -0400

    Implement evaluate for Coth

--- src/functions/unitary/Coth.java
+++ src/functions/unitary/Coth.java
@@ -14,7 +14,7 @@ public class Coth extends UnitaryFunction{
 
     @Override
     public double evaluate(double... variableValues) {
-        return 0;
+        return Math.cosh(function.evaluate(variableValues))/Math.sinh(function.evaluate(variableValues));
     }
 
     @Override

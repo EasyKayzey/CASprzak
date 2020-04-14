8096c40c4d41ce972e244723ffc4ef9df5a9f34c
michaelk522 <michaelkasprzak522@gmail.com>
2020-04-14 12:06:42 -0400

    Implement evaluate for Acsc

--- src/functions/unitary/Acsc.java
+++ src/functions/unitary/Acsc.java
@@ -23,6 +23,13 @@ public class Acsc extends UnitaryFunction {
 
     @Override
     public double evaluate(double... variableValues) {
-        return 0;
+        double functionEvaluated = function.evaluate(variableValues);
+        if (functionEvaluated > 1) {
+            return Math.acos(Math.sqrt(Math.pow(functionEvaluated, 2)-1)/functionEvaluated);
+        } else if (functionEvaluated < -1) {
+            return - Math.acos(Math.sqrt(Math.pow(functionEvaluated, 2)-1)/functionEvaluated);
+        } else {
+            return Double.NaN;
+        }
     }
 }

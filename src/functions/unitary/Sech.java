76a709b31e7f4889ad0a6087b9c078de66310f36
michaelk522 <michaelkasprzak522@gmail.com>
2020-04-14 13:37:26 -0400

    Create Sech class

---
+++ src/functions/unitary/Sech.java
@@ -0,0 +1,24 @@
+package functions.unitary;
+
+import functions.Function;
+
+public class Sech extends UnitaryFunction {
+    public Sech(Function function) {
+        super(function);
+    }
+
+    @java.lang.Override
+    public UnitaryFunction me(Function function) {
+        return null;
+    }
+
+    @java.lang.Override
+    public Function getDerivative(int varID) {
+        return null;
+    }
+
+    @java.lang.Override
+    public double evaluate(double... variableValues) {
+        return 0;
+    }
+}

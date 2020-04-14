1d3c6adcbe8eac5655c88952a9325efe345a4acd
michaelk522 <michaelkasprzak522@gmail.com>
2020-04-13 22:51:51 -0400

    Make JavaDoc for toString for Pow

--- src/CASprzak/BinaryFunctions/Pow.java
+++ src/CASprzak/BinaryFunctions/Pow.java
@@ -71,7 +71,10 @@ public class Pow extends BinaryFunction {
 		return new Pow(function1, function2);
 	}
 
-
+	/**
+	 * Returns a String representation of the Function
+	 * @return String representation of function
+	 */
 	@Override
 	public String toString() { //TODO this should probably be using StringBuilder
 		boolean parenF1 = !((function1 instanceof Constant) || (function1 instanceof Variable) || (function1 instanceof CommutativeFunction));

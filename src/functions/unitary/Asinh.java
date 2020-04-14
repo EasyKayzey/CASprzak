b4456d97c33627c27fcaf4e08e124d168c2e9f06
michaelk522 <michaelkasprzak522@gmail.com>
2020-04-14 13:40:46 -0400

    Create Asinh class

---
+++ src/functions/unitary/Asinh.java
@@ -0,0 +1,24 @@
+package functions.unitary;
+
+import functions.Function;
+
+public class Asinh extends UnitaryFunction {
+    public Asinh(Function function) {
+        super(function);
+    }
+
+    @Override
+    public UnitaryFunction me(Function function) {
+        return null;
+    }
+
+    @Override
+    public Function getDerivative(int varID) {
+        return null;
+    }
+
+    @Override
+    public double evaluate(double... variableValues) {
+        return 0;
+    }
+}

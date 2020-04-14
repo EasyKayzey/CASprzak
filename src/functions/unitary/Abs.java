9b515113a0ade91d70828208c2c5f0db6603008e
michaelk522 <michaelkasprzak522@gmail.com>
2020-04-14 11:31:52 -0400

    Create getDerivative for Abs

--- src/functions/unitary/Abs.java
+++ src/functions/unitary/Abs.java
@@ -1,6 +1,10 @@
 package functions.unitary;
 
 import functions.Function;
+import functions.binary.Pow;
+import functions.commutative.Add;
+import functions.commutative.Multiply;
+import functions.special.Constant;
 
 public class Abs extends UnitaryFunction {
     public Abs(Function function) {
@@ -12,4 +16,8 @@ public class Abs extends UnitaryFunction {
         return Math.abs(function.evaluate(variableValues));
     }
 
+    @Override
+    public Function getDerivative(int varID) {
+        return null;
+    }
 }

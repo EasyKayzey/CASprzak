20d0b97f05aa2e17f5a10309cffdcb5960d2b34b
michaelk522 <michaelkasprzak522@gmail.com>
2020-04-14 12:14:32 -0400

    Implement getDerivative for Asec

--- src/functions/unitary/Asec.java
+++ src/functions/unitary/Asec.java
@@ -1,6 +1,10 @@
 package functions.unitary;
 
 import functions.Function;
+import functions.binary.Pow;
+import functions.commutative.Add;
+import functions.commutative.Multiply;
+import functions.special.Constant;
 
 public class Asec extends UnitaryFunction {
     public Asec(Function function) {
@@ -8,17 +12,17 @@ public class Asec extends UnitaryFunction {
     }
 
     @Override
-    public UnitaryFunction me(Function operand) {
-        return new Asec(operand);
-    }
-
-    @Override
     public Function getDerivative(int varID) {
-        return null;
+        return new Multiply(function.getSimplifiedDerivative(varID), new Pow(new Constant(-1), new Multiply(new Abs(function), new Pow(new Constant(0.5), new Add(new Pow(new Constant(2), function), new Constant(-1))))));
     }
 
     @Override
     public double evaluate(double... variableValues) {
         return 0;
+    }
+
+    @Override
+    public UnitaryFunction me(Function operand) {
+        return new Asec(operand);
     }
 }

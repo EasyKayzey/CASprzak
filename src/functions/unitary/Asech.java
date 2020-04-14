3139e655df4a75f50b27904961a47bfb3f195dc2
michaelk522 <michaelkasprzak522@gmail.com>
2020-04-14 13:57:38 -0400

    Create Asech class

---
+++ src/functions/unitary/Asech.java
@@ -0,0 +1,24 @@
+package functions.unitary;
+
+import functions.Function;
+
+public class Asech extends UnitaryFunction {
+    public Asech(Function function) {
+        super(function);
+    }
+
+    @Override
+    public UnitaryFunction me(Function function) {
+        return null;
+    }
+
+    @Override
+    public Function getDerivative(int varID) {
+        return null;
+    }
+
+    @Override
+    public double evaluate(double... variableValues) {
+        return 0;
+    }
+}

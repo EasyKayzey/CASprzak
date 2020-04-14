29affaee3117eaac76f5f5ce4ed192bdee6b8109
eiis1000 <erez.m.israeli@gmail.com>
2020-04-14 18:00:55 -0400

    Refactor CASUI

--- src/ui/CASUI.java
+++ src/ui/CASUI.java
@@ -11,41 +11,43 @@ import java.util.Scanner;
 import java.util.regex.Pattern;
 
 public class CASUI {
+	private static final Pattern spacesMaybeComma = Pattern.compile(",?\\s*");
 	private static final Pattern commaSpaces = Pattern.compile(",\\s*");
 
 	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-		in.useDelimiter("\n");
+		Scanner scanner = new Scanner(System.in);
+		scanner.useDelimiter("\n");
 
 		System.out.println("What are your variables? Separate with spaces.");
-		String[] varsTemp = commaSpaces.split(in.next());
-		char[] vars = new char[varsTemp.length];
-		for (int i = 0; i < vars.length; i++) vars[i] = varsTemp[i].charAt(0);
+		String[] variableStrings = spacesMaybeComma.split(scanner.next());
+		char[] variables = new char[variableStrings.length];
+		for (int i = 0; i < variables.length; i++) variables[i] = variableStrings[i].charAt(0);
 
 		System.out.println("Enter your function to be stored:");
-		String raw = in.next();
+		String rawInput = scanner.next();
 
 		System.out.println("What are your inputs? Separate with commas, and order them with your variables.");
-		String[] visTemp = commaSpaces.split(in.next());
-		double[] vis = Arrays.stream(visTemp).mapToDouble(ConstantEvaluator::getConstant).toArray();
+		String[] inputStrings = commaSpaces.split(scanner.next());
+		double[] inputs = Arrays.stream(inputStrings).mapToDouble(ConstantEvaluator::getConstant).toArray();
 		System.out.println("Processing...");
 
-		PreProcessor.setVariables(vars);
+		Parser.setVariables(variables);
+		PreProcessor.setVariables(variables);
 
-		Function curFun = Parser.parse(PreProcessor.toPostfix(raw));
-		System.out.println("Here is your parsed function: " + curFun);
-		System.out.println("Here is the simplified toString of your function: " + curFun.simplifyTimes(10));
-		System.out.println("Here is your output: " + curFun.evaluate(vis));
+		Function currentFunction = Parser.parse(PreProcessor.toPostfix(rawInput));
+		System.out.println("Here is your parsed function: " + currentFunction);
+		System.out.println("Here is the simplified toString of your function: " + currentFunction.simplifyTimes(10));
+		System.out.println("Here is your output: " + currentFunction.evaluate(inputs));
 
 		System.out.println("Here is the derivative, simplified once:");
-		System.out.println(curFun.getSimplifiedDerivative(0));
+		System.out.println(currentFunction.getSimplifiedDerivative(0));
 		System.out.println("Here is the derivative, simplified completely:");
-		System.out.println(curFun.getSimplifiedDerivative(0).simplifyTimes(10));
+		System.out.println(currentFunction.getSimplifiedDerivative(0).simplifyTimes(10));
 
 		System.out.println("Here is the derivative, evaluated:");
-		System.out.println(curFun.getSimplifiedDerivative(0).simplifyTimes(10).evaluate(vis));
+		System.out.println(currentFunction.getSimplifiedDerivative(0).simplifyTimes(10).evaluate(inputs));
 
-		double solution = SingleVariableSolver.getSolutionPoint(curFun, -10);
+		double solution = SingleVariableSolver.getSolutionPoint(currentFunction, -10);
 		if (!Double.isNaN(solution)) {
 			System.out.println("Here is one zero for the expression:");
 			System.out.println(solution);
@@ -53,7 +55,7 @@ public class CASUI {
 			System.out.println("The algorithm did not find an initial solution.");
 		}
 
-		double[] solutions = SingleVariableSolver.getSolutionsRange(curFun, -10, 10);
+		double[] solutions = SingleVariableSolver.getSolutionsRange(currentFunction, -10, 10);
 		if (solutions.length > 0) {
 			System.out.println("Here are the zeros of the expression:");
 			System.out.println(Arrays.toString(solutions));

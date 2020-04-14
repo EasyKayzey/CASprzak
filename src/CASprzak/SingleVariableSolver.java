b5de692411f5a1cac1b2eeebce98f1d0540303d7
eiis1000 <erez.m.israeli@gmail.com>
2020-04-13 21:37:12 -0400

    Fix typos

--- src/CASprzak/SingleVariableSolver.java
+++ src/CASprzak/SingleVariableSolver.java
@@ -50,7 +50,7 @@ public class SingleVariableSolver {
      * Gives approximate roots of a {@link Function} using {@link #newtonsMethod} in a range of values after a specified amount of runs
      * @param expression the function whose roots are being found
      * @param lower the lower bound of the values that will be searched for roots
-     * @param upper the lower bound of the values that will be searched for roots
+     * @param upper the upper bound of the values that will be searched for roots
      * @param runs the amount of times that {@link #newtonsMethod} is ran recursively
      * @return an array of all the approximate roots found
      */
@@ -82,7 +82,7 @@ public class SingleVariableSolver {
      * Gives approximate roots of a {@link Function} using {@link #newtonsMethod} in a range of values after 1000 runs
      * @param expression the function whose roots are being found
      * @param lower the lower bound of the values that will be searched for roots
-     * @param upper the lower bound of the values that will be searched for roots
+     * @param upper the upper bound of the values that will be searched for roots
      * @return an array of all the approximate roots found
      */
     public double[] getSolutionsRange(Function expression, double lower, double upper) {

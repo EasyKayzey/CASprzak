809ea904d5d7967cefcacdacb1b4f456ab6f4c24
eiis1000 <erez.m.israeli@gmail.com>
2020-04-14 16:41:59 -0400

    Fix mistake in CASDebugger

--- src/ui/CASDebugger.java
+++ src/ui/CASDebugger.java
@@ -32,9 +32,9 @@ public class CASDebugger {
 				System.out.println("toString:");
 				System.out.println(function);
 				System.out.println("Simplified once:");
-				System.out.println(function = function.simplify());
+				System.out.println(function.simplify());
 				System.out.println("Simplified fully:");
-				System.out.println(function = function.simplifyTimes(10));
+				System.out.println(function.simplifyTimes(10));
 				System.out.println("Evaluate? Enter comma-separated input to evaluate or '!' otherwise.");
 				userInput = in.nextLine();
 				if (userInput.charAt(0) != '!') {

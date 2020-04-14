33d4a0251ff5b5c3a750060e552298b47c7fe7a8
eiis1000 <erez.m.israeli@gmail.com>
2020-04-13 21:22:33 -0400

    Begin documentation for Substitutable

--- src/CASprzak/Substitutable.java
+++ src/CASprzak/Substitutable.java
@@ -1,5 +1,12 @@
 package CASprzak;
 
 public interface Substitutable {
+
+	/**
+	 * Substitutes a new {@link Function} into a variable
+	 * @param varID the variable to be substituted into
+	 * @param toReplace the {@link Function} that will be substituted
+	 * @return the new {@link Function} after all substitutions are preformed
+	 */
 	Function substitute(int varID, Function toReplace);
 }

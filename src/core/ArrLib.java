8614954928c289c6406cbe88fe2195974317b4ce
eiis1000 <erez.m.israeli@gmail.com>
2020-04-15 13:17:53 -0400

    Implement ArrLib.factorial

--- src/core/ArrLib.java
+++ src/core/ArrLib.java
@@ -170,5 +170,19 @@ public class ArrLib {
 		return finalFunctionArray;
 	}
 
+	/**
+	 * Returns n factorial (n!)
+	 *
+	 * @param n the number
+	 * @return n!
+	 */
+	public static int factorial(int n) {
+		if (n < 0)
+			throw new IllegalArgumentException("Cannot take the factorial of a negative number.");
+		else if (n <= 1)
+			return 1;
+		else
+			return n * factorial(n - 1);
+	}
 
 }

410645778dc2c47d6fcdd30d26b7a7bf0f78bc91
eiis1000 <erez.m.israeli@gmail.com>
2020-04-15 12:02:23 -0400

    Fix split in CASUI

--- src/ui/CASUI.java
+++ src/ui/CASUI.java
@@ -11,15 +11,15 @@ import java.util.Scanner;
 import java.util.regex.Pattern;
 
 public class CASUI {
-	private static final Pattern spacesMaybeComma = Pattern.compile(",?\\s*");
-	private static final Pattern commaSpaces = Pattern.compile(",\\s*");
+	private static final Pattern commaSpaces = Pattern.compile("(?<=\\w),?\\s*(?=\\w)");
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		scanner.useDelimiter("\n");
 
-		System.out.println("What are your variables? Separate with spaces.");
-		String[] variableStrings = spacesMaybeComma.split(scanner.next());
+		System.out.println("What are your variables? Separate with commas and/or spaces.");
+		String[] variableStrings = commaSpaces.split(scanner.next());
+		System.out.println(Arrays.toString(variableStrings));
 		char[] variables = new char[variableStrings.length];
 		for (int i = 0; i < variables.length; i++)
 			variables[i] = variableStrings[i].charAt(0);
@@ -27,7 +27,7 @@ public class CASUI {
 		System.out.println("Enter your function to be stored:");
 		String rawInput = scanner.next();
 
-		System.out.println("What are your inputs? Separate with commas, and order them with your variables.");
+		System.out.println("What are your inputs? Separate with commas and/or spaces, and order them with your variables.");
 		String[] inputStrings = commaSpaces.split(scanner.next());
 		double[] inputs = Arrays.stream(inputStrings).mapToDouble(ConstantEvaluator::getConstant).toArray();
 		System.out.println("Processing...");

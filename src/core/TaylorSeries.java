9b8111a96f57563331c188a0cd009733dbf13788
michaelk522 <michaelkasprzak522@gmail.com>
2020-04-15 13:32:38 -0400

    Finish makeTaylorSeries

--- src/core/TaylorSeries.java
+++ src/core/TaylorSeries.java
@@ -1,13 +1,18 @@
 package core;
 
 import functions.Function;
+import functions.binary.Pow;
+import functions.commutative.Add;
+import functions.commutative.Multiply;
+import functions.special.Constant;
+import functions.special.Variable;
 
 public class TaylorSeries {
     public static Function makeTaylorSeries(Function function, int size) {
         Function[] taylorSeriesTerms = new Function[size];
         for (int i = 0; i < size; i++){
-            taylorSeriesTerms[i] = function.getNthDerivative(0, i);
+            taylorSeriesTerms[i] = new Multiply(new Constant(function.getNthDerivative(0, i).evaluate(0)/ArrLib.factorial(0)), new Pow(new Constant(i), new Variable(0)));
         }
-        return function;
+        return new Add(taylorSeriesTerms);
     }
 }

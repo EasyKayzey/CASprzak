e31fe24093cf0509910cdb253a7f0d9126d81677
eiis1000 <erez.m.israeli@gmail.com>
2020-04-20 18:27:26 -0400

    Document ConstantEvaluator

--- src/parsing/ConstantEvaluator.java
+++ src/parsing/ConstantEvaluator.java
@@ -4,6 +4,11 @@ public class ConstantEvaluator {
 
 	private ConstantEvaluator(){}
 
+	/**
+	 * Evaluates infix corresponding to a constant, like {@code pi/3}
+	 * @param infix infix string of constant
+	 * @return a double corresponding to the evaluated constant to be evaluated
+	 */
 	public static double getConstant(String infix) {
 		return Parser.parse(infix).evaluate();
 	}

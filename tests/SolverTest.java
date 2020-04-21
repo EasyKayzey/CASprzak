ba0889a8230e8e53583f37ef8a27be85c12c8256
michaelk522 <michaelkasprzak522@gmail.com>
2020-04-21 15:59:01 -0400

    Write more test for Solver

--- tests/SolverTest.java
+++ tests/SolverTest.java
@@ -4,6 +4,8 @@ import tools.singlevariable.Solver;
 import functions.Function;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SolverTest {
@@ -12,6 +14,24 @@ public class SolverTest {
     void simplePolynomial() {
         Function test = Parser.parse("x^2+-1");
         assertEquals(1, Solver.getSolutionPoint(test, 3));
+    }
+
+    @Test
+    void moreAdvancedPolynomial() {
+        Function test = Parser.parse("x^4-5x^2+4");
+        assertEquals(Arrays.toString(new double[]{-2, -1, 1, 2}), Arrays.toString(Solver.getSolutionsRange(test, -10, 10)));
+    }
+
+    @Test
+    void polynomialWithNoSolution() {
+        Function test = Parser.parse("x^2+1");
+        assertEquals(Double.NaN, Solver.getSolutionPoint(test, 4));
+    }
+
+    @Test
+    void polynomialWithNoSolutionRange() {
+        Function test = Parser.parse("x^2+1");
+        assertEquals(Arrays.toString(new double[]{}), Solver.getSolutionsRange(test, -10, 10));
     }
 
     @Test

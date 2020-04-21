770c6f025f6f328c3d0b21d3754d9771076fb87e
eiis1000 <erez.m.israeli@gmail.com>
2020-04-21 16:07:09 -0400

    Fix Solver.getSolutionsRange

--- src/tools/singlevariable/Solver.java
+++ src/tools/singlevariable/Solver.java
@@ -65,12 +65,15 @@ public class Solver {
 	public static double[] getSolutionsRange(Function expression, double lower, double upper, int runs) {
 		List<Double> solutions = SolverTools.createRange(upper, lower, 17);
 		solutions.forEach(solution -> getSolutionPoint(expression, solution, runs));
+		System.out.println(solutions);
 		ListIterator<Double> iter = solutions.listIterator();
 		double current;
 		while (iter.hasNext()) {
-			current = iter.next();
+			current = getSolutionPoint(expression, iter.next(), runs);
 			if (!(expression.evaluate(current) < 1E-3 && expression.evaluate(current) > -1E-3))
 				iter.remove();
+			else
+				iter.set(current);
 		}
 		SolverTools.nanRemover(solutions);
 		SolverTools.removeRepeatsInOrder(solutions);

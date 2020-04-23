532ea203983c8b0e72d3820f67632629ab2799a8
eiis1000 <erez.m.israeli@gmail.com>
2020-04-23 13:35:41 -0400

    Split up methods in KeywordInterface

--- src/ui/KeywordInterface.java
+++ src/ui/KeywordInterface.java
@@ -1,5 +1,9 @@
 package ui;
 
+import functions.Function;
+import functions.special.Variable;
+import parsing.Parser;
+
 import java.util.Arrays;
 import java.util.Scanner;
 import java.util.regex.Pattern;
@@ -7,6 +11,7 @@ import java.util.regex.Pattern;
 @SuppressWarnings("SpellCheckingInspection")
 public class KeywordInterface {
 	private static final Pattern spaces = Pattern.compile("\\s+");
+	public static final Pattern spacesOutsideQuotes = Pattern.compile("\\s+(?=[^\"]*(\"[^\"]*\"[^\"]*)*$)");
 	/**
 	 * A list of sets of keywords corresponding to operations
 	 */
@@ -29,18 +34,37 @@ public class KeywordInterface {
 		Scanner scan = new Scanner(System.in);
 		scan.useDelimiter("\\n");
 		//noinspection StatementWithEmptyBody
-		while (useKeywords(scan.next()));
+		while (userUseKeywords(scan.next()));
+	}
+
+	/**
+	 * Takes input as a string with command, arguments... and prints the result
+	 * @param input contains the command and arguments
+	 * @return false if exiting
+	 */
+	public static boolean userUseKeywords(String input) {
+		String[] splitInput = spaces.split(input, 2);
+		if ("exit".equals(splitInput[0]))
+			return false;
+		else {
+			try {
+				System.out.println(useKeywords(input));
+			} catch (IllegalArgumentException e) {
+				System.out.println(e.getMessage());
+				return false;
+			}
+		}
+		return true;
 	}
 
 	/**
 	 * Takes input as a string with command, arguments...
 	 * @param input contains the command and arguments
+	 * @return the {@link functions.Function} requested
 	 */
-	public static boolean useKeywords(String input) {
+	public static Function useKeywords(String input) {
 		String[] splitInput = spaces.split(input, 2);
-		if ("exit".equals(splitInput[0]))
-			return false;
-		else if (Arrays.asList(keywordSets[0]).contains(splitInput[0]))
+		if (Arrays.asList(keywordSets[0]).contains(splitInput[0]))
 			pd(splitInput[1]);
 		else if (Arrays.asList(keywordSets[1]).contains(splitInput[0]))
 			eval(splitInput[1]);
@@ -57,33 +81,41 @@ public class KeywordInterface {
 		else if (Arrays.asList(keywordSets[7]).contains(splitInput[0]))
 			sto(splitInput[1]);
 		else {
-			System.out.println(splitInput[0] + " is not supported by KeywordInterface");
-			return false;
+			throw new IllegalArgumentException(splitInput[0] + " is not supported by KeywordInterface");
 		}
-		return true;
+		return null;
 	}
 
-	private static void pd(String input) {
+	public static Function pd(String input) {
+		String[] splitInput = spacesOutsideQuotes.split(input);
+		return Parser.parse(splitInput[1]).getSimplifiedDerivative(Variable.getVarID(splitInput[0].charAt(0)));
 	}
 
-	private static void eval(String input) {
+	public static Function eval(String input) {
+		String[] splitInput = spacesOutsideQuotes.split(input);
 	}
 
-	private static void simp(String input) {
+	public static Function simp(String input) {
+		String[] splitInput = spacesOutsideQuotes.split(input);
 	}
 
-	private static void sub(String input) {
+	public static Function sub(String input) {
+		String[] splitInput = spacesOutsideQuotes.split(input);
 	}
 
-	private static void sol(String input) {
+	public static Function sol(String input) {
+		String[] splitInput = spacesOutsideQuotes.split(input);
 	}
 
-	private static void ext(String input) {
+	public static Function ext(String input) {
+		String[] splitInput = spacesOutsideQuotes.split(input);
 	}
 
-	private static void tay(String input) {
+	public static Function tay(String input) {
+		String[] splitInput = spacesOutsideQuotes.split(input);
 	}
 
-	private static void sto(String input) {
+	public static Function sto(String input) {
+		String[] splitInput = spacesOutsideQuotes.split(input);
 	}
 }

c5b86333cbd0d9efd2a7a2e559abcdb0509255ff
eiis1000 <erez.m.israeli@gmail.com>
2020-04-24 19:35:23 -0400

    Implement CommandUI.output

--- src/ui/CommandUI.java
+++ src/ui/CommandUI.java
@@ -2,6 +2,7 @@ package ui;
 
 import parsing.KeywordInterface;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class CommandUI extends KeywordInterface {
@@ -19,13 +20,27 @@ public class CommandUI extends KeywordInterface {
 				flag = false;
 			else {
 				try {
-					System.out.println(useKeywords(input));
+					output(useKeywords(input));
 					flag = true;
 				} catch (IllegalArgumentException e) {
 					System.out.println(e.getMessage());
 					flag = false;
 				}
 			}
+		}
+	}
+
+	@SuppressWarnings("ChainOfInstanceofChecks")
+	private static void output(Object object) {
+		if (object != null) {
+			if (object instanceof double[] array)
+				System.out.println(Arrays.toString(array));
+			else if (object instanceof Object[] array)
+				System.out.println(Arrays.toString(array));
+			else
+				System.out.println(object);
+		} else {
+			System.out.println("Done");
 		}
 	}
 }

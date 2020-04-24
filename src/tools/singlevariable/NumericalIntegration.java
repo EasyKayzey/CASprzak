56f935a9999bafb95e18b3304490ce5d830220b1
eiis1000 <erez.m.israeli@gmail.com>
2020-04-24 11:17:46 -0400

    Implement NumericalIntegration.simpsonsError

--- src/tools/singlevariable/NumericalIntegration.java
+++ src/tools/singlevariable/NumericalIntegration.java
@@ -5,7 +5,6 @@ import functions.Function;
 
 public class NumericalIntegration {
 
-
     /**
      * Returns the approximate definite integral of a {@link Function} function on a range
      * @param function The {@link Function} whose integral is being found
@@ -28,5 +27,17 @@ public class NumericalIntegration {
         sum += function.evaluate(x);
         sum *= step/3;
         return sum;
+    }
+
+    /**
+     * Returns the approximate definite integral of a {@link Function} function on a range
+     * @param function The {@link Function} whose integral is being found
+     * @param lowerBound The lower bound of the range
+     * @param upperBound The upper bound of the range
+     * @return the approximate definite integral of function on a range
+     */
+    public static double simpsonsError(Function function, double lowerBound, double upperBound) {
+        Function fourthDerivative = function.getNthDerivative(0, 4);
+        return fourthDerivative.evaluate(Extrema.findLocalMaxima(fourthDerivative, lowerBound, upperBound))*Math.pow(upperBound - lowerBound, 5) / (180 * Math.pow(Settings.amountOfSegments, 4));
     }
 }

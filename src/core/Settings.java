1848aac55058f819f2b45e07f84bdb7929883a02
michaelk522 <michaelkasprzak522@gmail.com>
2020-04-24 17:45:44 -0400

    Remove the Random see from Settings

--- src/core/Settings.java
+++ src/core/Settings.java
@@ -12,5 +12,4 @@ public class Settings {
 	public static boolean distributeExponents = true;
 	public static boolean cacheDerivatives = true;
 	public static boolean trustImmutability = true; // Makes it so that getFunctions and other getters don't return clone()
-	public static long seed = 2345235;
 }

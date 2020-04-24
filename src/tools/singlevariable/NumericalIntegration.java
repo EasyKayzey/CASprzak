6d65ca24fe551242bac290152cf7ec5b8353c259
michaelk522 <michaelkasprzak522@gmail.com>
2020-04-24 00:02:24 -0400

    Add documentation to NumericalIntegration

--- src/tools/singlevariable/NumericalIntegration.java
+++ src/tools/singlevariable/NumericalIntegration.java
@@ -3,9 +3,19 @@ package tools.singlevariable;
 import functions.Function;
 
 public class NumericalIntegration {
+    /**
+     * The amount of segments the region can be broken into (MUST BE EVEN)
+     */
     public static final double amountOfSegments = 500;
 
 
+    /**
+     * Returns the approximate definite integral of a {@link Function} function on a range
+     * @param function The {@link Function} whose integral is being found
+     * @param lowerBound The lower bound of the range
+     * @param upperBound The upper bound of the range
+     * @return the approximate definite integral of function on a range
+     */
     public static double simpsonsRule(Function function, double lowerBound, double upperBound) {
         double sum = function.evaluate(lowerBound);
         double step = (upperBound-lowerBound)/amountOfSegments;

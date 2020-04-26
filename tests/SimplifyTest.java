4b7fc16bfbfae2ba0a14cd6b5ad739d5e04a3798
michaelk522 <michaelkasprzak522@gmail.com>
2020-04-25 23:29:26 -0400

    Move tests from EqualsTest to SimplifyTest

--- tests/SimplifyTest.java
+++ tests/SimplifyTest.java
@@ -1,4 +1,6 @@
+import core.Settings;
 import functions.Function;
+import functions.commutative.Multiply;
 import org.junit.jupiter.api.Test;
 import parsing.Parser;
 
@@ -41,4 +43,76 @@ public class SimplifyTest {
         Function test2 = Parser.parse("1+12*x^-1");
         assertEquals(test1, test2);
     }
+
+    @Test
+    void distributeTerms() {
+        Function test1 = Parser.parse("sin(x)*(1+5x)");
+        Function test2 = Parser.parse("sin(x)+5*x*sin(x)");
+        assertEquals(((Multiply)test1).distributeAll(), test2);
+    }
+
+    @Test
+    void distributeExponents() {
+        Settings.distributeExponents = true;
+        Function test1 = Parser.parse("(2xy)^2");
+        Function test2 = Parser.parse("4x^2y^2");
+        assertEquals(test1, test2);
+    }
+
+    @Test
+    void addExponents() {
+        Function test1 = Parser.parse("x^2*x^4");
+        Function test2 = Parser.parse("x^6");
+        assertEquals(test1, test2);
+    }
+
+    @Test
+    void combineLikeTermsMultExample() {
+        Function test1 = Parser.parse("(3+x)(2+x)(1+x)");
+        Function test2 = Parser.parse("x^3+11x+6x^2+6");
+        assertEquals(test1, test2);
+    }
+
+    @Test
+    void simplifySimpleExponents() {
+        Function test1 = Parser.parse("(x+1)^1");
+        Function test2 = Parser.parse("x+1");
+        assertEquals(test1, test2);
+    }
+
+    @Test
+    void simplifyMultiplyExponents() {
+        Function test1 = Parser.parse("(x^3)^2");
+        Function test2 = Parser.parse("x^6");
+        assertEquals(test1, test2);
+    }
+
+    @Test
+    void simplifyIdentity() {
+        Function test1 = Parser.parse("x+0");
+        Function test2 = Parser.parse("x*1");
+        assertEquals(test1, test2);
+    }
+
+    @Test
+    void simplifyPullAdd() {
+        Function test1 = Parser.parse("(x+(y+z))");
+        Function test2 = Parser.parse("x+y+z");
+        assertEquals(test1, test2);
+    }
+
+    @Test
+    void simplifyPullMultiply() {
+        Function test1 = Parser.parse("x*(yz)");
+        Function test2 = Parser.parse("x*y*z");
+        assertEquals(test1, test2);
+    }
+
+    @Test
+    void timesZero() {
+        Function test1 = Parser.parse("x*0");
+        Function test2 = Parser.parse("0");
+        assertEquals(test1, test2);
+    }
+
 }

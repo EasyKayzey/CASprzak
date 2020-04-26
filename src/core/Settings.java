d8d517b50488849cab9741b7cf89b3bc96d59812
eiis1000 <erez.m.israeli@gmail.com>
2020-04-25 21:28:18 -0400

    Fix TaylorSeries evaluate

--- src/core/Settings.java
+++ src/core/Settings.java
@@ -12,4 +12,5 @@ public class Settings {
 	public static boolean distributeExponents = true;
 	public static boolean cacheDerivatives = true;
 	public static boolean trustImmutability = true; // Makes it so that getFunctions and other getters don't return clone()
+	public static char singleVariableDefault = 'x';
 }

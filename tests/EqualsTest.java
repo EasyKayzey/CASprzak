9a1d93a044dd8b8e030118b5c6a9dc0ba2640fb0
michaelk522 <michaelkasprzak522@gmail.com>
2020-04-25 23:28:13 -0400

    Move tests from EqualsTest to SimplifyTest

--- tests/EqualsTest.java
+++ tests/EqualsTest.java
@@ -45,41 +45,6 @@ public class EqualsTest {
     }
 
     @Test
-    void equalWhenSimplified() {
-        Function test1 = Parser.parse("x+(1+3-2)*1");
-        Function test2 = Parser.parse("x+2");
-        assertEquals(test1, test2);
-    }
-
-    @Test
-    void constantEquality() {
-        Function test1, test2;
-        test1 = Parser.parse("1");
-        test2 = Parser.parse("0+1");
-        assertEquals(test1, test2);
-        test1 = Parser.parse("1");
-        test2 = Parser.parse("2");
-        assertNotEquals(test1, test2);
-        test1 = Parser.parse("e");
-        test2 = Parser.parse("" + Math.E);
-        assertEquals(test1, test2);
-    }
-
-    @Test
-    void simplifiesDivisionExponents() {
-        Function test1 = Parser.parse("x^3/x^2");
-        Function test2 = Parser.parse("x");
-        assertEquals(test1, test2);
-    }
-
-    @Test
-    void combineLikeTerms() {
-        Function test1 = Parser.parse("3*x^2+5*x^-1+7*x^-1-3*x^2+1");
-        Function test2 = Parser.parse("1+12*x^-1");
-        assertEquals(test1, test2);
-    }
-
-    @Test
     void switchedOrderVariableConstant() {
         Function test1 = Parser.parse("1+2x");
         Function test2 = Parser.parse("2x+1");
--- tests/SimplifyTest.java
+++ tests/SimplifyTest.java
@@ -1,2 +1,44 @@
+import functions.Function;
+import org.junit.jupiter.api.Test;
+import parsing.Parser;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
 public class SimplifyTest {
+
+    @Test
+    void equalWhenSimplified() {
+        Function test1 = Parser.parse("x+(1+3-2)*1");
+        Function test2 = Parser.parse("x+2");
+        assertEquals(test1, test2);
+    }
+
+    @Test
+    void constantEquality() {
+        Function test1, test2;
+        test1 = Parser.parse("1");
+        test2 = Parser.parse("0+1");
+        assertEquals(test1, test2);
+        test1 = Parser.parse("1");
+        test2 = Parser.parse("2");
+        assertNotEquals(test1, test2);
+        test1 = Parser.parse("e");
+        test2 = Parser.parse("" + Math.E);
+        assertEquals(test1, test2);
+    }
+
+    @Test
+    void simplifiesDivisionExponents() {
+        Function test1 = Parser.parse("x^3/x^2");
+        Function test2 = Parser.parse("x");
+        assertEquals(test1, test2);
+    }
+
+    @Test
+    void combineLikeTerms() {
+        Function test1 = Parser.parse("3*x^2+5*x^-1+7*x^-1-3*x^2+1");
+        Function test2 = Parser.parse("1+12*x^-1");
+        assertEquals(test1, test2);
+    }
 }

7d47abc1c4f4c443308f786ef25bb53bfd1042d8
michaelk522 <michaelkasprzak522@gmail.com>
2020-04-27 12:56:06 -0400

    Add tests for isPolynomial

--- tests/PolynomialTest.java
+++ tests/PolynomialTest.java
@@ -3,6 +3,8 @@ import org.junit.jupiter.api.Test;
 import parsing.Parser;
 import tools.PolynomialTools;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
 public class PolynomialTest {
     @Test
     void isSimpleMonomial() {
@@ -32,5 +34,23 @@ public class PolynomialTest {
     void isGeneralMonomial() {
         Function test = Parser.parse("3.32145*x^(-4.4352)");
         assert(PolynomialTools.isGeneralMonomial(test));
+    }
+
+    @Test
+    void isSimplePolynomialOfAMonomial() {
+        Function test = Parser.parse("3x^2");
+        assert(PolynomialTools.isPolynomial(test));
+    }
+
+    @Test
+    void isNotSimplePolynomial() {
+        Function test = Parser.parse("5x^-1");
+        assertFalse(PolynomialTools.isPolynomial(test));
+    }
+
+    @Test
+    void isSimplePolynomial() {
+        Function test = Parser.parse("5x^3+x^2+5x+3269485");
+        assert(PolynomialTools.isPolynomial(test));
     }
 }

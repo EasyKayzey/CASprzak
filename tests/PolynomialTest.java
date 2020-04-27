35367e895c76de1d329c3b9b95acc505e5e506f4
michaelk522 <michaelkasprzak522@gmail.com>
2020-04-27 12:48:07 -0400

    Finish test for isMonomial

--- tests/PolynomialTest.java
+++ tests/PolynomialTest.java
@@ -9,4 +9,23 @@ public class PolynomialTest {
         Function test = Parser.parse("x");
         assert(PolynomialTools.isMonomial(test));
     }
+
+    @Test
+    void isSimpleMonomialWithAConstant() {
+        Function test = Parser.parse("3x");
+        assert(PolynomialTools.isMonomial(test));
+    }
+
+    @Test
+    void isSimpleMonomialWithAPower() {
+        Function test = Parser.parse("x^2");
+        assert(PolynomialTools.isMonomial(test));
+    }
+
+    @Test
+    void isSimpleMonomialWithPowerAndConstant() {
+        Function test = Parser.parse("3x^2");
+        assert(PolynomialTools.isMonomial(test));
+    }
+
 }

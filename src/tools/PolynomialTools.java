a393a01856912e6677ea526e5c8f0668398edf7b
eiis1000 <erez.m.israeli@gmail.com>
2020-04-27 12:26:50 -0400

    Create PolynomialTools

---
+++ src/tools/PolynomialTools.java
@@ -0,0 +1,24 @@
+package tools;
+
+import functions.Function;
+
+public class PolynomialTools {
+
+	/**
+	 * Checks if a given {@link Function} is a polynomial
+	 * @param function the function to be checked
+	 * @return true if function is a polynomial
+	 */
+	public static boolean isPolynomial(Function function) {
+		return false;
+	}
+
+	/**
+	 * Checks if a given {@link Function} is a monomial
+	 * @param function the function to be checked
+	 * @return true if function is a monomial
+	 */
+	public static boolean isMonomial(Function function) {
+		return false;
+	}
+}

9de7e0e17da23eebbe6bd07a5b20854e0853da21
michaelk522 <michaelkasprzak522@gmail.com>
2020-04-27 13:05:00 -0400

    Switch asserts with assertTrues

--- tests/PolynomialTest.java
+++ tests/PolynomialTest.java
@@ -4,42 +4,43 @@ import parsing.Parser;
 import tools.PolynomialTools;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class PolynomialTest {
     @Test
     void isSimpleMonomial() {
         Function test = Parser.parse("x");
-        assert(PolynomialTools.isMonomial(test));
+        assertTrue(PolynomialTools.isMonomial(test));
     }
 
     @Test
     void isSimpleMonomialWithAConstant() {
         Function test = Parser.parse("3x");
-        assert(PolynomialTools.isMonomial(test));
+        assertTrue(PolynomialTools.isMonomial(test));
     }
 
     @Test
     void isSimpleMonomialWithAPower() {
         Function test = Parser.parse("x^2");
-        assert(PolynomialTools.isMonomial(test));
+        assertTrue(PolynomialTools.isMonomial(test));
     }
 
     @Test
     void isSimpleMonomialWithPowerAndConstant() {
         Function test = Parser.parse("3x^2");
-        assert(PolynomialTools.isMonomial(test));
+        assertTrue(PolynomialTools.isMonomial(test));
     }
 
     @Test
     void isGeneralMonomial() {
         Function test = Parser.parse("3.32145*x^(-4.4352)");
-        assert(PolynomialTools.isGeneralMonomial(test));
+        assertTrue(PolynomialTools.isGeneralMonomial(test));
     }
 
     @Test
     void isSimplePolynomialOfAMonomial() {
         Function test = Parser.parse("3x^2");
-        assert(PolynomialTools.isPolynomial(test));
+        assertTrue(PolynomialTools.isPolynomial(test));
     }
 
     @Test
@@ -51,7 +52,7 @@ public class PolynomialTest {
     @Test
     void isSimplePolynomial() {
         Function test = Parser.parseSimplified("5x^3+x^2+5x+3269485");
-        assert(PolynomialTools.isPolynomial(test));
+        assertTrue(PolynomialTools.isPolynomial(test));
     }
 
 }

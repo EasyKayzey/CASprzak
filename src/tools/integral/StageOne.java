fa6928b1ff72e12bf5a844609a692ed71b6a2a24
michaelk522 <michaelkasprzak522@gmail.com>
2020-04-28 18:20:16 -0400

    Create stageOne class

---
+++ src/tools/integral/StageOne.java
@@ -0,0 +1,11 @@
+package tools.integral;
+
+import functions.Function;
+
+public class StageOne {
+    public static Function derivativeDivides(Function function, char variableChar) {
+
+
+        return function;
+    }
+}

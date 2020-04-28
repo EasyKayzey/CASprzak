5b64a0c2fc0a9f3f3adf813eb2e9c915f8ebbbff
michaelk522 <michaelkasprzak522@gmail.com>
2020-04-28 18:51:33 -0400

    Implement integrate splits Adds

--- src/tools/integral/Integral.java
+++ src/tools/integral/Integral.java
@@ -1,6 +1,8 @@
 package tools.integral;
 
 import functions.Function;
+import functions.commutative.Add;
+import functions.special.Constant;
 
 import java.util.Map;
 
@@ -61,5 +63,16 @@ public class Integral extends Function {
 	@Override
 	public Function simplify() {
 		return null; //TODO implement
+	}
+
+	public Function integrate() {
+		if (integrand instanceof Add terms) {
+			Function[] integratedTerms = new Function[terms.getFunctionsLength()];
+			for(int i = 0; i < terms.getFunctionsLength(); i++) {
+				integratedTerms[i] = new Integral(terms.getFunctions()[i], respectTo);
+			}
+			return new Add(integratedTerms);
+		}
+		return new Constant("0");
 	}
 }

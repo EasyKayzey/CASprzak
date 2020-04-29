ced783045744014157958f0c688ae0b9280e651b
michaelk522 <michaelkasprzak522@gmail.com>
2020-04-28 22:26:16 -0400

    Create stripConstants

--- src/tools/integral/IntegralsTools.java
+++ src/tools/integral/IntegralsTools.java
@@ -2,19 +2,31 @@ package tools.integral;
 
 import functions.Function;
 import functions.commutative.Multiply;
+import functions.special.Constant;
+import tools.FunctionTools;
+import tools.helperclasses.Pair;
 
 import java.util.AbstractMap;
 import java.util.HashMap;
 import java.util.Map;
 
 public class IntegralsTools {
-    public static Map.Entry<Double, Function> stripConstants(Function function) {
+    public static Pair<Double, Function> stripConstants(Function function) {
         if (!(function instanceof Multiply))
-            return new AbstractMap.SimpleEntrty(1.0, function);
+            return new Pair(1.0, function);
         else if (function instanceof Multiply multiply) {
-            Function[] terms = multiply.getFunctions();
+            Function[] terms = multiply.simplifyConstants().getFunctions();
             double constant = 1;
-
+            Function[] termsWithConstantRemoved = terms;
+            for (int i = 0; i < multiply.getFunctions().length; i++) {
+                if (terms[i] instanceof Constant number) {
+                    constant *= number.constant;
+                    termsWithConstantRemoved = FunctionTools.removeFunctionAt(terms, i);
+                }
+            }
+            return new Pair(constant, (new Multiply(termsWithConstantRemoved)).simplifyPull());
+        } else {
+            throw new RuntimeException("This should never happen");
         }
     }
 }

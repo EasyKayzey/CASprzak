5ae28f6682bbb93ea6ea5c0cac614156cea95b17
eiis1000 <erez.m.israeli@gmail.com>
2020-04-29 13:12:47 -0400

    Fix base case in SearchTools

--- src/tools/SearchTools.java
+++ src/tools/SearchTools.java
@@ -10,9 +10,14 @@ public class SearchTools {
 	}
 
 	public static boolean existsExcluding(Function input, Predicate<? super Function> test, Predicate<? super Function> exclude) {
-		for (Function f : input)
-			if (!exclude.test(f) && (test.test(f) || existsExcluding(f, test, exclude)))
-				return true;
+		if (exclude.test(input))
+			return false;
+		else if (test.test(input))
+			return true;
+		else
+			for (Function f : input)
+				if (existsExcluding(f, test, exclude))
+					return true;
 		return false;
 	}
 }

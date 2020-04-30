6e3932b1821f841de749da11d00d9307fc841b1f
eiis1000 <erez.m.israeli@gmail.com>
2020-04-30 18:48:00 -0400

    Add X, Y, Z in DefaultFunctions

--- src/tools/DefaultFunctions.java
+++ src/tools/DefaultFunctions.java
@@ -4,17 +4,22 @@ import functions.Function;
 import functions.binary.Pow;
 import functions.commutative.Product;
 import functions.special.Constant;
+import functions.special.Variable;
 
 @SuppressWarnings("StaticVariableOfConcreteClass")
 public class DefaultFunctions {
 	public static final Constant PI = new Constant("pi");
 	public static final Constant E = new Constant("e");
-	public static final Constant ZERO = new Constant(0);
 	public static final Constant NEGATIVE_ONE = new Constant(-1);
+	public static final Constant ZERO = new Constant(0);
 	public static final Constant ONE = new Constant(1);
 	public static final Constant TWO = new Constant(2);
 	public static final Constant HALF = new Constant(.5);
 	public static final Constant NEGATIVE_HALF = new Constant(-.5);
+
+	public static final Variable X = new Variable('x');
+	public static final Variable Y = new Variable('y');
+	public static final Variable Z = new Variable('z');
 
 	public static Product negative(Function input) {
 		return new Product(NEGATIVE_ONE, input);

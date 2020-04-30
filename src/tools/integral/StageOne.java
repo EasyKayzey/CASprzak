046609e2d13c9fbb5f0864175dfad8fd12609bdc
michaelk522 <michaelkasprzak522@gmail.com>
2020-04-30 17:20:53 -0400

    Implemented other easy integrals in StageOne

--- src/tools/integral/StageOne.java
+++ src/tools/integral/StageOne.java
@@ -1,12 +1,15 @@
 package tools.integral;
 
 import functions.Function;
+import functions.binary.Logb;
 import functions.binary.Pow;
 import functions.commutative.Product;
 import functions.commutative.Sum;
 import functions.special.Constant;
 import functions.special.Variable;
 import functions.unitary.Ln;
+import functions.unitary.trig.Cos;
+import functions.unitary.trig.Sin;
 import tools.SearchTools;
 import tools.helperclasses.Pair;
 
@@ -23,9 +26,18 @@ public class StageOne<function> {
             } else if (function instanceof Pow power && power.getFunction1() instanceof Constant constant1 && power.getFunction2().getSimplifiedDerivative(variableChar) instanceof Constant constant2) {
                 number /= constant2.constant;
                 return power(number, constant1.constant, power.getFunction2());
-            } else if (function instanceof Ln log && log.operand().getSimplifiedDerivative(variableChar) instanceof Constant constant1) {
+            } else if (function instanceof Ln log && log.operand.getSimplifiedDerivative(variableChar) instanceof Constant constant1) {
                 number /= constant1.constant;
-                return naturalLog(number, log.operand());
+                return naturalLog(number, log.operand);
+            } else if (function instanceof Logb logb && logb.getFunction2() instanceof Constant constant1 && logb.getFunction1().getSimplifiedDerivative(variableChar) instanceof Constant constant2) {
+                number /= (constant2.constant * Math.log(constant1.constant));
+                return naturalLog(number, logb.getFunction1());
+            } else if (function instanceof Sin sin && sin.operand.getSimplifiedDerivative(variableChar) instanceof Constant constant1) {
+                number /= constant1.constant;
+                return sin(number, sin.operand);
+            } else if (function instanceof Cos cos && cos.operand.getSimplifiedDerivative(variableChar) instanceof Constant constant1) {
+                number /= constant1.constant;
+                return cos(number, cos.operand);
             }
         }
 
@@ -57,5 +69,13 @@ public class StageOne<function> {
 
     private static Function naturalLog(double number, Function operand) {
         return new Product(new Constant(number), new Sum(new Product(operand, new Ln(operand)), new Product(new Constant(-1), operand)));
+    }
+
+    private static Function sin(double number, Function operand) {
+        return new Product(new Constant(-1*number), new Cos(operand));
+    }
+
+    private static Function cos(double number, Function operand) {
+        return new Product(new Constant(number), new Sin(operand));
     }
 }

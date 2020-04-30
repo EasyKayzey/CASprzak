7be4b9a68ebf896c272257b1751ed11c209d785c
michaelk522 <michaelkasprzak522@gmail.com>
2020-04-30 14:56:22 -0400

    Small fixes in IntegralTools

--- src/tools/integral/IntegralsTools.java
+++ src/tools/integral/IntegralsTools.java
@@ -2,19 +2,15 @@ package tools.integral;
 
 import functions.Function;
 import functions.commutative.Product;
-import functions.commutative.Sum;
 import functions.special.Constant;
 import tools.FunctionTools;
 import tools.helperclasses.Pair;
 
-import java.util.AbstractMap;
-import java.util.HashMap;
-import java.util.Map;
 
 public class IntegralsTools {
     public static Pair<Double, Function> stripConstants(Function function) {
         if (!(function instanceof Product))
-            return new Pair<Double, Function>(1.0, function);
+            return new Pair<>(1.0, function);
         else if (function instanceof Product multiply) {
             Function[] terms = multiply.simplifyConstants().getFunctions();
             double constant = 1;
@@ -26,9 +22,9 @@ public class IntegralsTools {
                 }
             }
             if (termsWithConstantRemoved.length == 1)
-                return new Pair<Double, Function>(constant, termsWithConstantRemoved[0]);
+                return new Pair<>(constant, termsWithConstantRemoved[0]);
             else
-                return new Pair<Double, Function>(constant, (new Product(termsWithConstantRemoved)).simplifyPull());
+                return new Pair<>(constant, (new Product(termsWithConstantRemoved)).simplifyPull());
         } else {
             throw new RuntimeException("This should never happen");
         }

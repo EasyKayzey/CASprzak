5a09d86d7de20ab66d420c68cf75efc2c7929187
eiis1000 <erez.m.israeli@gmail.com>
2020-04-30 17:47:17 -0400

    Comment up SearchTools

--- src/tools/SearchTools.java
+++ src/tools/SearchTools.java
@@ -5,10 +5,24 @@ import tools.helperclasses.FunctionPredicate;
 
 @SuppressWarnings("unused")
 public class SearchTools {
+
+	/**
+	 * Returns true if a {@link Function} in the function tree satisfies the predicate test
+	 * @param input the Function to be searched in
+	 * @param test the predicate to be tested
+	 * @return true if found
+	 */
 	public static boolean exists(Function input, FunctionPredicate test) {
 		return existsExcluding(input, test, (a -> false));
 	}
 
+	/**
+	 * Returns true if a {@link  Function} in the function tree satisfies the predicate test, excluding all Functions on branches satisfying the predicate exclude
+	 * @param input the Function to be searched in
+	 * @param test the predicate to be tested
+	 * @param exclude the predicate to exclude
+	 * @return true if found
+	 */
 	public static boolean existsExcluding(Function input, FunctionPredicate test, FunctionPredicate exclude) {
 		if (exclude.test(input))
 			return false;
@@ -21,10 +35,24 @@ public class SearchTools {
 		return false;
 	}
 
+
+	/**
+	 * Returns true if a direct child of this {@link Function} satisfies the predicate test
+	 * @param input the function to be searched in
+	 * @param test the predicate to be tested
+	 * @return true if found
+	 */
 	public static boolean existsSurface(Function input, FunctionPredicate test) {
 		return existsSurfaceExcluding(input, test, (a -> false));
 	}
 
+	/**
+	 * Returns true if a direct child of this {@link  Function} satisfies the predicate test, excluding all Functions satisfying the predicate exclude
+	 * @param input the Function to be searched in
+	 * @param test the predicate to be tested
+	 * @param exclude the predicate to exclude
+	 * @return true if found
+	 */
 	public static boolean existsSurfaceExcluding(Function input, FunctionPredicate test, FunctionPredicate exclude) {
 		if (exclude.test(input))
 			return false;
@@ -34,4 +62,5 @@ public class SearchTools {
 					return true;
 		return false;
 	}
+
 }

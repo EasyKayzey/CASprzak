db30953fff16484c4c95f08b3f41298c41280b32
eiis1000 <erez.m.israeli@gmail.com>
2020-04-30 12:00:52 -0400

    Implement AbstractPair

---
+++ src/tools/helperclasses/AbstractPair.java
@@ -0,0 +1,6 @@
+package tools.helperclasses;
+
+public interface AbstractPair<T, U> {
+	T getFirst();
+	U getSecond();
+}
--- src/tools/helperclasses/Pair.java
+++ src/tools/helperclasses/Pair.java
@@ -1,11 +1,21 @@
 package tools.helperclasses;
 
-public class Pair<T, U> {
+public class Pair<T, U> implements AbstractPair<T, U>{
 	public final T first;
 	public final U second;
 
 	public Pair(T first, U second) {
 		this.first = first;
 		this.second = second;
+	}
+
+	@Override
+	public T getFirst() {
+		return null;
+	}
+
+	@Override
+	public U getSecond() {
+		return null;
 	}
 }

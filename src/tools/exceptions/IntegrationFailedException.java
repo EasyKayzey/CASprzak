7485c117cd212ae1e958fec71f58d521f0e086af
eiis1000 <erez.m.israeli@gmail.com>
2020-05-01 13:23:49 -0400

    Add IntegrationFailedException

---
+++ src/tools/exceptions/IntegrationFailedException.java
@@ -0,0 +1,7 @@
+package tools.exceptions;
+
+public class IntegrationFailedException extends UnsupportedOperationException{
+	public IntegrationFailedException(String message) {
+		super(message);
+	}
+}

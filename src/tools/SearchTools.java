9d25bab111e5ceea144909bd5a67ab290d682474
eiis1000 <erez.m.israeli@gmail.com>
2020-05-01 11:22:12 -0400

    Make SearchTools.existsInOppositeSurfaceSubset more flexible

--- src/tools/SearchTools.java
+++ src/tools/SearchTools.java
@@ -120,7 +120,7 @@ public class SearchTools {
 			for (int ix = 0; ix < functions.length; ix++)
 				if (((~run >> ix) & 1) > 0)
 					subset.add(functions[ix]);
-			if (existsExcluding(input.me(subset.toArray(new Function[0])), test, excludeFromSearch))
+			if (test.test(input.me(subset.toArray(new Function[0]))))
 				return true;
 		}
 		return false;

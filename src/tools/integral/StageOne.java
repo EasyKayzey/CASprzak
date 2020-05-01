49d90e6507f0f23be65ccab5ac9d086a4bdfac22
eiis1000 <erez.m.israeli@gmail.com>
2020-05-01 10:21:12 -0400

    Make StageOne use SearchTools

--- src/tools/integral/StageOne.java
+++ src/tools/integral/StageOne.java
@@ -33,7 +33,7 @@ public class StageOne {
                     Function derivativeWithoutConstant = derivative.second;
                     double constantInFront = derivative.first;
                     Product derivativeTimesOperation = new Product(derivativeWithoutConstant, f);
-                    if (SearchTools.existsSurface(product, (u -> u.equals(derivativeWithoutConstant))) && !SearchTools.existsInOppositeSurfaceSubset((CommutativeFunction) function, (u -> (u instanceof Variable v) && (v.varID == variableChar)), (u -> u.equals(derivativeTimesOperation)))) {
+                    if (SearchTools.existsSurface(product, (u -> u.equals(derivativeWithoutConstant))) && !SearchTools.existsInOppositeSurfaceSubset((CommutativeFunction) function, SearchTools.isVariable(variableChar), (u -> u.equals(derivativeTimesOperation)))) {
                         number /= constantInFront;
                         return exponential(number, base.constant, power.getFunction1());
                     }
@@ -43,7 +43,7 @@ public class StageOne {
                     Function derivativeWithoutConstant = derivative.second;
                     double constantInFront = derivative.first;
                     Product derivativeTimesOperation = new Product(derivativeWithoutConstant, f);
-                    if (SearchTools.existsSurface(product, (u -> u.equals(derivativeWithoutConstant))) && !SearchTools.existsInOppositeSurfaceSubset((CommutativeFunction) function, (u -> (u instanceof Variable v) && (v.varID == variableChar)), (u -> u.equals(derivativeTimesOperation)))) {
+                    if (SearchTools.existsSurface(product, (u -> u.equals(derivativeWithoutConstant))) && !SearchTools.existsInOppositeSurfaceSubset((CommutativeFunction) function, SearchTools.isVariable(variableChar), (u -> u.equals(derivativeTimesOperation)))) {
                         number /= constantInFront;
                         return power(number, exponent.constant, power.getFunction2());
                     }
@@ -53,7 +53,7 @@ public class StageOne {
                     Function derivativeWithoutConstant = derivative.second;
                     double constantInFront = derivative.first;
                     Product derivativeTimesOperation = new Product(derivativeWithoutConstant, f);
-                    if (SearchTools.existsSurface(product, (u -> u.equals(derivativeWithoutConstant))) && !SearchTools.existsInOppositeSurfaceSubset((CommutativeFunction) function, (u -> (u instanceof Variable v) && (v.varID == variableChar)), (u -> u.equals(derivativeTimesOperation)))) {
+                    if (SearchTools.existsSurface(product, (u -> u.equals(derivativeWithoutConstant))) && !SearchTools.existsInOppositeSurfaceSubset((CommutativeFunction) function, SearchTools.isVariable(variableChar), (u -> u.equals(derivativeTimesOperation)))) {
                         number /= constantInFront;
                         return naturalLog(number, ln.operand);
                     }
@@ -63,7 +63,7 @@ public class StageOne {
                     Function derivativeWithoutConstant = derivative.second;
                     double constantInFront = derivative.first;
                     Product derivativeTimesOperation = new Product(derivativeWithoutConstant, f);
-                    if (SearchTools.existsSurface(product, (u -> u.equals(derivativeWithoutConstant))) && !SearchTools.existsInOppositeSurfaceSubset((CommutativeFunction) function, (u -> (u instanceof Variable v) && (v.varID == variableChar)), (u -> u.equals(derivativeTimesOperation)))) {
+                    if (SearchTools.existsSurface(product, (u -> u.equals(derivativeWithoutConstant))) && !SearchTools.existsInOppositeSurfaceSubset((CommutativeFunction) function, SearchTools.isVariable(variableChar), (u -> u.equals(derivativeTimesOperation)))) {
                         number /= (constantInFront * Math.log(constant1.constant));
                         return naturalLog(number, logb.getFunction2());
                     }
@@ -73,7 +73,7 @@ public class StageOne {
                     Function derivativeWithoutConstant = derivative.second;
                     double constantInFront = derivative.first;
                     Product derivativeTimesOperation = new Product(derivativeWithoutConstant, f);
-                    if (SearchTools.existsSurface(product, (u -> u.equals(derivativeWithoutConstant))) && !SearchTools.existsInOppositeSurfaceSubset((CommutativeFunction) function, (u -> (u instanceof Variable v) && (v.varID == variableChar)), (u -> u.equals(derivativeTimesOperation)))) {
+                    if (SearchTools.existsSurface(product, (u -> u.equals(derivativeWithoutConstant))) && !SearchTools.existsInOppositeSurfaceSubset((CommutativeFunction) function, SearchTools.isVariable(variableChar), (u -> u.equals(derivativeTimesOperation)))) {
                         number /= (constantInFront);
                        return unitaryFunctionSwitchCase(unit.getClass().getSimpleName().toLowerCase(), unit.operand, number);
                     }
@@ -83,7 +83,7 @@ public class StageOne {
                 Function derivativeWithoutConstant = derivative.second;
                 double constantInFront = derivative.first;
                 Product derivativeTimesOperation = new Product(derivativeWithoutConstant, f);
-                if (SearchTools.existsSurface(product, (u -> u.equals(derivativeWithoutConstant))) && !SearchTools.existsInOppositeSurfaceSubset((CommutativeFunction) function, (u -> (u instanceof Variable v) && (v.varID == variableChar)), (u -> u.equals(derivativeTimesOperation)))) {
+                if (SearchTools.existsSurface(product, (u -> u.equals(derivativeWithoutConstant))) && !SearchTools.existsInOppositeSurfaceSubset((CommutativeFunction) function, SearchTools.isVariable(variableChar), (u -> u.equals(derivativeTimesOperation)))) {
                     number /= (constantInFront);
                     return power(number, 1, f);
                 }

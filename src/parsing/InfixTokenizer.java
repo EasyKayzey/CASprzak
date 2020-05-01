b9886721f997c127bb5244d4e6a7290c50755c04
eiis1000 <erez.m.israeli@gmail.com>
2020-04-30 22:44:12 -0400

    Write SolverTest.chemTest2

--- src/parsing/InfixTokenizer.java
+++ src/parsing/InfixTokenizer.java
@@ -38,7 +38,7 @@ public class InfixTokenizer {
 			"|(?<=[()])|(?=[()]))" +						// Splits if preceded or followed by a parenthesis
 			"(?<![ .])(?![ .])" +							// The PREVIOUS FOUR LINES ONLY WORK if not preceded or followed by a dot or space
 			"|(?<=[CP])|(?=[CP])" +							// Splits if preceded or followed by C or P
-			"|(?<=[\\D-])(?=\\.)"							// Splits if preceded by [a character that isn't a digit or -] and followed by a dot
+			"|(?<=[A-Za-z(\\-])(?=\\.)"						// Splits if preceded by [a letter, (, or -] and followed by a dot
 	);
 
 	private InfixTokenizer(){}
@@ -61,12 +61,14 @@ public class InfixTokenizer {
 		infix = subtractionFinder.matcher(infix).replaceAll("+-").replace("/", "*/");
 		// Turns expressions like xyz into x*y*z
 		infix = parseVariablePairs(infix);
+		System.out.println(infix);
 		// Adds parentheses to enforce order of operations
 		infix = "((((" + times.matcher(plus.matcher(closeParen.matcher(openParen.matcher(infix).replaceAll("((((")).replaceAll("))))")).replaceAll("))+((")).replaceAll(")*(") + "))))";
 		// Splits infix into tokens
 		List<String> tokens = infixSplitter.splitAsStream(infix).collect(Collectors.toCollection(LinkedList::new));
 		// Move postfix operations like ! to infix
 		movePostfix(tokens);
+		System.out.println(tokens);
 		// Return tokens
 		return tokens;
 	}

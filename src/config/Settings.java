df5dbd0c998494d8b95de1fd97e8b3da4097a3f9
eiis1000 <erez.m.israeli@gmail.com>
2020-05-03 17:47:23 -0400

    Reorder things in Settings

--- src/config/Settings.java
+++ src/config/Settings.java
@@ -6,6 +6,7 @@ public class Settings {
 	//	public static boolean debug = true;
 	public static int defaultSolverIterations = 100;
 	public static int defaultRangeSections = 29;
+	public static char singleVariableDefault = 'x';
 	public static double simpsonsSegments = 500; // MUST BE EVEN
 	public static double zeroMargin = 1e-3;
 	public static double integerMargin = 1e-4;
@@ -16,7 +17,6 @@ public class Settings {
 	public static boolean trustImmutability = true; // Makes it so that getFunctions and other getters don't return clone()
 	public static boolean enforceIntegerOperations = true; // Makes it so that combinatorial operations return integers
 	public static boolean exitSolverOnProximity = false; // Makes it so that solvers exit when f(x) is within equalsMargin of 0
-	public static char singleVariableDefault = 'x';
 	public static SolverType defaultSolverType = SolverType.NEWTON;
 	public static FactorialType defaultFactorial = FactorialType.RECURSIVE;
 }

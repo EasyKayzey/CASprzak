bb421879b22febd68614b2612c67b37fd42a5569
michaelk522 <michaelkasprzak522@gmail.com>
2020-05-03 00:40:30 -0400

    Add documentation to IntegralTools

--- src/tools/integration/IntegralsTools.java
+++ src/tools/integration/IntegralsTools.java
@@ -8,6 +8,12 @@ import tools.helperclasses.Pair;
 
 
 public class IntegralsTools {
+
+    /**
+     * Strips a {@link Function} of any Constants and returns a {@link Pair} of the constant and the stripped Function
+     * @param function The Function whose Constant is being Stripped
+     * @return A {@link Pair} of the constant and the stripped Function
+     */
     public static Pair<Double, Function> stripConstants(Function function) {
         if (function instanceof Product multiply) {
             Function[] terms = multiply.simplifyConstants().getFunctions();

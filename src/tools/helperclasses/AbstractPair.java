e9203ac858a3f56a52fcc6e9b6908b00a06f4d91
eiis1000 <erez.m.israeli@gmail.com>
2020-05-03 15:55:31 -0400

    Implement AbstractPair.toString

--- src/tools/helperclasses/AbstractPair.java
+++ src/tools/helperclasses/AbstractPair.java
@@ -3,4 +3,8 @@ package tools.helperclasses;
 public abstract class AbstractPair<T, U> {
 	public abstract T getFirst();
 	public abstract U getSecond();
+
+	public String toString() {
+		return "<" + getFirst() + ", " + getSecond() + ">";
+	}
 }

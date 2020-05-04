7e5a7d7b1fb81f3c955f966a5635c068bae9de76
michaelk522 <michaelkasprzak522@gmail.com>
2020-05-04 19:10:43 -0400

    Fix typo

--- src/tools/helperclasses/AbstractMutablePair.java
+++ src/tools/helperclasses/AbstractMutablePair.java
@@ -1,6 +1,6 @@
 package tools.helperclasses;
 
-public abstract class AbstractMutablePair<T, U> extends AbstractPair<T, U> {
+public abstract class AbstractMutablePair<T, U> extends AbstractPair<T, U> { //TODO nothing in this folder is documented
 	public abstract void setFirst(T first);
 	public abstract void setSecond(U second);
 }

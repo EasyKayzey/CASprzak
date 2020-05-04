bf90317ff50f23c74bfe2c7b3443b32291377ad0
eiis1000 <erez.m.israeli@gmail.com>
2020-05-04 18:15:58 -0400

    Edit NumericalIntegration

--- src/tools/singlevariable/NumericalIntegration.java
+++ src/tools/singlevariable/NumericalIntegration.java
@@ -2,6 +2,7 @@ package tools.singlevariable;
 
 import config.Settings;
 import functions.GeneralFunction;
+import tools.SearchTools;
 
 import java.util.Map;
 
@@ -12,22 +13,25 @@ public class NumericalIntegration {
      * @param function The {@link GeneralFunction} whose integral is being found
      * @param lowerBound The lower bound of the range
      * @param upperBound The upper bound of the range
-     * @return the approximate definite integral of function on a range
+     * @return the approximate definite integral of the function on a range
      */
     public static double simpsonsRule(GeneralFunction function, double lowerBound, double upperBound) {
-        double sum = function.evaluate(Map.of(Settings.singleVariableDefault, lowerBound));
-        double step = (upperBound-lowerBound)/ Settings.simpsonsSegments;
+        if (Settings.simpsonsSegments % 2 != 0)
+            throw new IllegalArgumentException("Amount of segments for Simpson's rule must be even.");
+        char var = SearchTools.getSingleVariable(function);
+        double step = (upperBound - lowerBound) / Settings.simpsonsSegments;
         double x = lowerBound + step;
-        for (int i = 1; i < Settings.simpsonsSegments /2; i++) {
-            sum += 4*function.evaluate(Map.of(Settings.singleVariableDefault, x));
+        double sum = function.evaluate(Map.of(var, lowerBound));
+        for (int i = 1; i < Settings.simpsonsSegments / 2; i++) {
+            sum += 4 * function.evaluate(Map.of(var, x));
             x += step;
-            sum += 2*function.evaluate(Map.of(Settings.singleVariableDefault, x));
+            sum += 2 * function.evaluate(Map.of(var, x));
             x += step;
         }
-        sum += 4*function.evaluate(Map.of(Settings.singleVariableDefault, x));
+        sum += 4*function.evaluate(Map.of(var, x));
         x += step;
-        sum += function.evaluate(Map.of(Settings.singleVariableDefault, x));
-        sum *= step/3;
+        sum += function.evaluate(Map.of(var, x));
+        sum *= step / 3;
         return sum;
     }
 
@@ -36,11 +40,12 @@ public class NumericalIntegration {
      * @param function The {@link GeneralFunction} whose integral is being found
      * @param lowerBound The lower bound of the range
      * @param upperBound The upper bound of the range
-     * @return the maximum error associated with the definite integral of function on a range
+     * @return the maximum error associated with the definite integral of the function on a range
      */
     public static double simpsonsError(GeneralFunction function, double lowerBound, double upperBound) {
-        GeneralFunction fourthDerivative = function.getNthDerivative(Settings.singleVariableDefault, 4);
-        return fourthDerivative.evaluate(Map.of(Settings.singleVariableDefault, Extrema.findMaximumOnRange(fourthDerivative, lowerBound, upperBound)))*Math.pow(upperBound - lowerBound, 5) / (180 * Math.pow(Settings.simpsonsSegments, 4));
+        char var = SearchTools.getSingleVariable(function);
+        GeneralFunction fourthDerivative = function.getNthDerivative(var, 4);
+        return fourthDerivative.evaluate(Map.of(var, Extrema.findMaximumOnRange(fourthDerivative, lowerBound, upperBound))) * Math.pow(upperBound - lowerBound, 5) / (180 * Math.pow(Settings.simpsonsSegments, 4));
     }
 
     /**
@@ -48,7 +53,7 @@ public class NumericalIntegration {
      * @param function The {@link GeneralFunction} whose integral is being found
      * @param lowerBound The lower bound of the range
      * @param upperBound The upper bound of the range
-     * @return the approximate definite integral of function on a range with an error range
+     * @return a double array comprised of the approximate definite integral of the function on a range and its error range
      */
     public static double[] simpsonsRuleWithError(GeneralFunction function, double lowerBound, double upperBound) {
         return new double[]{simpsonsRule(function, lowerBound, upperBound), simpsonsError(function, lowerBound, upperBound)};

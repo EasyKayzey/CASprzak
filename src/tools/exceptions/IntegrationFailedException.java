e5feb2a9e3a24716e4bbdf17c27a1080c86838bb
eiis1000 <erez.m.israeli@gmail.com>
2020-05-05 09:39:56 -0400

    Document exceptions

--- src/tools/exceptions/IntegrationFailedException.java
+++ src/tools/exceptions/IntegrationFailedException.java
@@ -1,6 +1,10 @@
 package tools.exceptions;
 
 public class IntegrationFailedException extends UnsupportedOperationException {
+	/**
+	 * To be thrown when integration has failed
+	 * @param message more information about failed integration
+	 */
 	public IntegrationFailedException(String message) {
 		super(message);
 	}

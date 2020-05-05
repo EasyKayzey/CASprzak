49f4e9ff7ed40627a6840882c6c770eed8fbc76f
eiis1000 <erez.m.israeli@gmail.com>
2020-05-05 13:15:26 -0400

    Fix whitespace and move lists to the end of Parser

--- src/parsing/Parser.java
+++ src/parsing/Parser.java
@@ -11,67 +11,8 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class Parser {
-	/**
-	 * A list of unitary operations
-	 */
-	public static final List<String> unitaryOperations = new LinkedList<>(){
-		{
-			add("-");
-			add("/");
-			add("!");
-			add("sin");
-			add("cos");
-			add("tan");
-			add("log");
-			add("ln");
-			add("sqrt");
-			add("exp");
-			add("abs");
-			add("sign");
-			add("dirac");
-			add("sin");
-			add("cos");
-			add("tan");
-			add("csc");
-			add("sec");
-			add("cot");
-			add("asin");
-			add("acos");
-			add("atan");
-			add("acsc");
-			add("asec");
-			add("acot");
-			add("sinh");
-			add("cosh");
-			add("tanh");
-			add("csch");
-			add("sech");
-			add("coth");
-			add("asinh");
-			add("acosh");
-			add("atanh");
-			add("acsch");
-			add("asech");
-			add("acoth");
-		}
-	};
-
-	/**
-	 * A list of binary operations
-	 */
-	public static final List<String> binaryOperations = new LinkedList<>() {
-		{
-			add("^");
-			add("*");
-			add("+");
-			add("logb");
-			add("C");
-			add("P");
-		}
-	};
 
 	private Parser(){}
-
 
 	/**
 	 * Parses infix using {@link parsing.PreProcessor} and {@link #parse(List)}
@@ -145,4 +86,62 @@ public class Parser {
 		return parse(infix).evaluate(null);
 	}
 
+	/**
+	 * A list of unitary operations
+	 */
+	public static final List<String> unitaryOperations = new LinkedList<>() {
+		{
+			add("-");
+			add("/");
+			add("!");
+			add("sin");
+			add("cos");
+			add("tan");
+			add("log");
+			add("ln");
+			add("sqrt");
+			add("exp");
+			add("abs");
+			add("sign");
+			add("dirac");
+			add("sin");
+			add("cos");
+			add("tan");
+			add("csc");
+			add("sec");
+			add("cot");
+			add("asin");
+			add("acos");
+			add("atan");
+			add("acsc");
+			add("asec");
+			add("acot");
+			add("sinh");
+			add("cosh");
+			add("tanh");
+			add("csch");
+			add("sech");
+			add("coth");
+			add("asinh");
+			add("acosh");
+			add("atanh");
+			add("acsch");
+			add("asech");
+			add("acoth");
+		}
+	};
+
+	/**
+	 * A list of binary operations
+	 */
+	public static final List<String> binaryOperations = new LinkedList<>() {
+		{
+			add("^");
+			add("*");
+			add("+");
+			add("logb");
+			add("C");
+			add("P");
+		}
+	};
 }

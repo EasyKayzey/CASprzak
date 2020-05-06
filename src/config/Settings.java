7e61cef523eccadc19250522a20c57605dbb699b
eiis1000 <erez.m.israeli@gmail.com>
2020-05-06 17:45:34 -0400

    Add TODO in Settings

--- src/config/Settings.java
+++ src/config/Settings.java
@@ -78,7 +78,7 @@ public class Settings {
 	/**
 	 * Denotes whether or not function immutability should be trusted when using getters or performing simplifications. There should be no reason to turn this off.
 	 */
-	public static boolean trustImmutability = true;
+	public static boolean trustImmutability = true; // TODO REMOVE THIS!!!!
 
 	/**
 	 * Forces functions in {@link functions.unitary.combo} to return integers when using approximations

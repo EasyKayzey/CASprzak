08ac6749cc119ad16a8c1a6cde57fe5b98d64a37
eiis1000 <erez.m.israeli@gmail.com>
2020-05-06 09:58:46 -0400

    Extend Evaluable documentation

--- src/functions/Evaluable.java
+++ src/functions/Evaluable.java
@@ -5,8 +5,8 @@ import java.util.Map;
 public interface Evaluable {
 
 	/**
-	 * Evaluates a {@link GeneralFunction} at a point
-	 * @param variableValues the values of the variables of the {@link GeneralFunction} at the point
+	 * Evaluates a {@link GeneralFunction} at a point denoted by a hashmap
+	 * @param variableValues the values of the variables in the {@link GeneralFunction} at the point
 	 * @return the value of the {@link GeneralFunction} at the point
 	 */
 	double evaluate(Map<Character, Double> variableValues);

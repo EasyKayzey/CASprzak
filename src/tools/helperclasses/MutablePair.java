c7733b04ef6b26c4a6296ae8a51f69154a681118
eiis1000 <erez.m.israeli@gmail.com>
2020-05-08 16:12:57 -0400

    Document constructors in pair classes

--- src/tools/helperclasses/MutablePair.java
+++ src/tools/helperclasses/MutablePair.java
@@ -1,17 +1,37 @@
 package tools.helperclasses;
 
+import java.util.Map;
+
 public class MutablePair<T, U> extends AbstractMutablePair<T, U> {
 	public T first;
 	public U second;
 
+	/**
+	 * Creates a mutable pair by specifying its values
+	 * @param first first value in the pair
+	 * @param second second value in the pair
+	 */
 	public MutablePair(T first, U second) {
 		this.first = first;
 		this.second = second;
 	}
 
+	/**
+	 * Creates a mutable pair using another pair
+	 * @param pair the pair containing the values to be used
+	 */
 	public MutablePair(AbstractPair<T, U> pair) {
 		this.first = pair.getFirst();
 		this.second = pair.getSecond();
+	}
+
+	/**
+	 * Creates a mutable pair using a map entry
+	 * @param entry the entry containing the values to be used
+	 */
+	public MutablePair(Map.Entry<T, U> entry) {
+		this.first = entry.getKey();
+		this.second = entry.getValue();
 	}
 
 	@Override

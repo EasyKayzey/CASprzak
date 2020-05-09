b1562420c925ca9643ef049f7b15dffa3846a86a
michaelk522 <michaelkasprzak522@gmail.com>
2020-05-09 19:30:38 -0400

    Remove TODO in Sum

--- src/functions/commutative/Sum.java
+++ src/functions/commutative/Sum.java
@@ -75,7 +75,7 @@ public class Sum extends CommutativeFunction {
 	 * @return a {@link Sum} where like terms are added together
 	 */
 	public Sum combineLikeTerms() {
-		GeneralFunction[] combinedTerms = ArrayTools.deepClone(functions);// TODO possibly make this use StripConstants or something similar
+		GeneralFunction[] combinedTerms = ArrayTools.deepClone(functions);
 		for (int a = 0; a < combinedTerms.length; a++)
 			if (!(combinedTerms[a] instanceof Product product && product.getFunctions()[0] instanceof Constant))
 				combinedTerms[a] = new Product(DefaultFunctions.ONE, combinedTerms[a]).simplifyPull();

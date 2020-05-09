f61bb6c8634d895e336540d3103fcd0232bcd7a7
Michael Kasprzak <michaelkasprzak522@gmail.com>
2020-05-09 14:17:47 -0400

    Document InverseTrigFunction

--- src/functions/unitary/trig/inverse/InverseTrigFunction.java
+++ src/functions/unitary/trig/inverse/InverseTrigFunction.java
@@ -8,11 +8,18 @@ import functions.unitary.trig.GeneralTrigFunction;
 import tools.DefaultFunctions;
 
 public abstract class InverseTrigFunction extends GeneralTrigFunction {
+	/**
+	 * Constructs a new InverseTrigFunction
+	 * @param operand The operand of the InverseTrigFunction
+	 */
 	public InverseTrigFunction(GeneralFunction operand) {
 		super(operand);
 	}
 
-	//TODO document
+	/**
+	 * Returns a {@link GeneralFunction} that would be the integral of the function with {@link UnitaryFunction#operand} as the variable
+	 * @return a {@link GeneralFunction} that would be the integral of the function with {@link UnitaryFunction#operand} as the variable
+	 */
 	public GeneralFunction getElementaryIntegral() {
 		return new Sum(new Product(operand, me(operand)), new Product(DefaultFunctions.NEGATIVE_ONE, UnitaryFunction.newInstanceOf(getInverse(), me(operand))));
 	}

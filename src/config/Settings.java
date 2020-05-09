be6e0e930fc16c7dc970293a8ddbc416eceec763
eiis1000 <erez.m.israeli@gmail.com>
2020-05-08 23:15:30 -0400

    Remove default settings in Setting, leave settings in config

--- src/config/Settings.java
+++ src/config/Settings.java
@@ -29,80 +29,80 @@ public class Settings {
 	/**
 	 * The amount of times that {@link tools.singlevariable.Solver} will run unless the exit conditions are met beforehand
 	 */
-	public static int defaultSolverIterations = 100;
+	public static int defaultSolverIterations;
 
 	/**
 	 * The amount of segments which {@link tools.singlevariable.Solver} splits a given range into
 	 */
-	public static int defaultRangeSections = 29;
+	public static int defaultRangeSections;
 
 	/**
 	 * The number of segments that {@link tools.singlevariable.NumericalIntegration#simpsonsRule(GeneralFunction, double, double)} uses when performing numerical integration. **MUST BE EVEN**
 	 */
-	public static int simpsonsSegments = 500; // MUST BE EVEN
+	public static int simpsonsSegments; // MUST BE EVEN
 
 	/**
 	 * The variable to be used in {@link tools.singlevariable} when none is specified
 	 */
-	public static char singleVariableDefault = 'x';
+	public static char singleVariableDefault;
 
 	/**
 	 * The margin that {@link tools.singlevariable.Solver} uses to determine if a values is close enough to zero to considered zero
 	 */
-	public static double zeroMargin = 1e-3;
+	public static double zeroMargin;
 
 	/**
 	 * The margin used in {@link ParsingTools#toInteger(double)} when deciding when a double is close enough to an integer
 	 */
-	public static double integerMargin = 1e-4;
+	public static double integerMargin;
 
 	/**
 	 * The default margin to be used when checking if two doubles are equal
 	 */
-	public static double equalsMargin = 1e-12;
+	public static double equalsMargin;
 
 	/**
 	 * Denotes whether or not expressions like sin(pi/2) must be escaped to \sin(\pi/2). Enabling this will significantly reduce issues.
 	 */
-	public static boolean enforceEscapes = false;
+	public static boolean enforceEscapes;
 
 	/**
 	 * Denotes whether functions of constants should be simplified, e.g. \sin(\pi/2) -> 1
 	 */
-	public static boolean simplifyFunctionsOfConstants = true;
+	public static boolean simplifyFunctionsOfConstants;
 
 	/**
 	 * Denotes whether exponents should be distributed over multiplication in a normal simplify(), e.g. (2x)^2 -> 4x^2
 	 */
-	public static boolean distributeExponents = true;
+	public static boolean distributeExponents;
 
 	/**
 	 * Denotes whether or not the derivatives of functions should be cached when created
 	 */
-	public static boolean cacheDerivatives = true;
+	public static boolean cacheDerivatives;
 
 	/**
 	 * Forces functions in {@link functions.unitary.combo} to return integers when using approximations
 	 */
-	public static boolean enforceIntegerOperations = true;
+	public static boolean enforceIntegerOperations;
 
 	/**
 	 * Denotes whether methods in {@link tools.singlevariable} should exit if the result is within a certain proximity of the target. Improves performance at the cost of accuracy.
 	 */
-	public static boolean exitSolverOnProximity = false;
+	public static boolean exitSolverOnProximity;
 
 	/**
 	 * Denotes whether transforms should execute their action when simplify is called
 	 */
-	public static boolean executeOnSimplify = true;
+	public static boolean executeOnSimplify;
 
 	/**
 	 * Denotes the default method used to solve equations
 	 */
-	public static SolverType defaultSolverType = SolverType.NEWTON;
+	public static SolverType defaultSolverType;
 
 	/**
 	 * Denotes the default implementation of factorial to be used
 	 */
-	public static FactorialType defaultFactorial = FactorialType.RECURSIVE;
+	public static FactorialType defaultFactorial;
 }

e5e296dac37ebe76f0de180e9504608b59ec5361
eiis1000 <erez.m.israeli@gmail.com>
2020-05-11 16:47:22 -0400

    Remove print statement in test

--- tests/LatexTest.java
+++ tests/LatexTest.java
@@ -19,8 +19,6 @@ public class LatexTest {
 	@Test
 	void testVar() {
 		GeneralFunction test = FunctionParser.parseInfix("\\pi\\Gamma");
-		System.out.println(test);
-		System.out.println(Map.of(ParsingTools.getCharacter("\\Gamma"), 3.2));
 		assertEquals(10, test.evaluate(Map.of(ParsingTools.getCharacter("\\Gamma"), 3.2)), .3);
 	}
 

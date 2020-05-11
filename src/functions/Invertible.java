c536a6d7c6fefbdd36cb5fbba2b5268da41f4b32
Michael Kasprzak <michaelkasprzak522@gmail.com>
2020-05-11 17:27:01 -0400

    Fix documentation in Invertible

--- src/functions/Invertible.java
+++ src/functions/Invertible.java
@@ -3,7 +3,7 @@ package functions;
 public interface Invertible {
 
 	/**
-	 * Returns the Class corresponding to the inverse of this trig function
+	 * Returns the Class corresponding to the inverse of this function
 	 * @return the inverse class
 	 */
 	Class<?> getInverse();

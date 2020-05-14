22dd1ba25f8449b7bfd0eb2efe84b9c5296419ce
eiis1000 <erez.m.israeli@gmail.com>
2020-05-14 19:09:36 -0400

    Add code braces to Differentiable javadoc

--- src/functions/Differentiable.java
+++ src/functions/Differentiable.java
@@ -5,15 +5,15 @@ public interface Differentiable {
 	/**
 	 * Returns the derivative of a {@link GeneralFunction} with respect to a variable. Should not be used in general; {@link GeneralFunction#getSimplifiedDerivative(char)} is strongly preferred.
 	 * @param varID the ID of the variable that is differentiated against
-	 * @return the derivative of the function with respect to varID
+	 * @return the derivative of the function with respect to {@code varID}
 	 */
 	GeneralFunction getDerivative(char varID);
 
 	/**
-	 * Returns the Nth derivative of a {@link GeneralFunction} with respect to a variable
+	 * Returns the {@code N}th derivative of a {@link GeneralFunction} with respect to a variable
 	 * @param varID the ID of the variable that is differentiated against
-	 * @param N the amount of times to be differentiated
-	 * @return the derivative of the function with respect to varID
+	 * @param N the amount of times to differentiate
+	 * @return the {@code N}th derivative of the function with respect to {@code varID}
 	 */
 	@SuppressWarnings("unused")
 	GeneralFunction getNthDerivative(char varID, int N);

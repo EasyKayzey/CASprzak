939f128ffb1087d6abcc6819a66ae26de7f47e42
eiis1000 <erez.m.israeli@gmail.com>
2020-05-14 17:01:22 -0400

    Edit SearchTools and fix javadoc

--- src/tools/SearchTools.java
+++ src/tools/SearchTools.java
@@ -10,8 +10,10 @@ import java.util.function.Predicate;
 
 public class SearchTools {
 
+	private SearchTools(){}
+
 	/**
-	 * Returns true if any {@link GeneralFunction} in the function tree satisfies the predicate test
+	 * Returns true if any {@link GeneralFunction} in the function tree satisfies {@code test}
 	 * @param input the {@link GeneralFunction} to be searched in
 	 * @param test the predicate to be tested
 	 * @return true if found
@@ -21,10 +23,10 @@ public class SearchTools {
 	}
 
 	/**
-	 * Returns true if a {@link  GeneralFunction} in the function tree satisfies the predicate test, excluding all Functions on branches satisfying the predicate exclude
+	 * Returns true if a {@link  GeneralFunction} in the function tree satisfies {@code test}, excluding all functions on branches whose parent satisfies {@code exclude}
 	 * @param input the {@link GeneralFunction} to be searched in
 	 * @param test the predicate to be tested
-	 * @param exclude the predicate to exclude
+	 * @param exclude the predicate to be excluded
 	 * @return true if found
 	 */
 	public static boolean existsExcluding(GeneralFunction input, Predicate<? super GeneralFunction> test, Predicate<? super GeneralFunction> exclude) {
@@ -41,49 +43,46 @@ public class SearchTools {
 
 
 	/**
-	 * Returns true if a single direct child of this {@link GeneralFunction} satisfies the predicate test
+	 * Returns true if at least one direct child of this {@link GeneralFunction} satisfies {@code test}
 	 * @param input the function to be searched in
 	 * @param test the predicate to be tested
 	 * @return true if found
 	 */
 	public static boolean existsSurface(GeneralFunction input, Predicate<? super GeneralFunction> test) {
-		return existsSurfaceExcluding(input, test, a -> false);
-	}
-
-	/**
-	 * Returns true if a single direct child of this {@link  GeneralFunction} satisfies the predicate test, excluding all Functions satisfying the predicate exclude
-	 * @param input the {@link GeneralFunction} to be searched in
-	 * @param test the predicate to be tested
-	 * @param exclude the predicate to exclude
-	 * @return true if found
-	 */
-	public static boolean existsSurfaceExcluding(GeneralFunction input, Predicate<? super GeneralFunction> test, Predicate<? super GeneralFunction> exclude) {
-		if (exclude.test(input))
-			return false;
-		else
-			for (GeneralFunction f : input)
-				if (test.test(f))
-					return true;
+		for (GeneralFunction f : input)
+			if (test.test(f))
+				return true;
 		return false;
 	}
 
 
 	/**
-	 * Checks if this {@link CommutativeFunction} has a subset (as a new instance of its class) satisfying the condition, including empty and single-element products
+	 * Checks if this {@link CommutativeFunction} has a subset (as a new instance of its class) satisfying {@code test}, including empty and single-element functions
 	 * @param input the {@link CommutativeFunction} to be searched in
 	 * @param test the condition to be satisfied
 	 * @return true if the condition was satisfied by a subset
 	 */
 	public static boolean existsInSurfaceSubset(CommutativeFunction input, Predicate<? super GeneralFunction> test) {
-		return getSubsetIDs(input, test).size() > 0;
+		GeneralFunction[] functions = input.getFunctions();
+		for (int run = 0; run < Math.pow(2, functions.length); run++) {
+			List<GeneralFunction> subset = new ArrayList<>();
+			for (int ix = 0; ix < functions.length; ix++)
+				if (((run >> ix) & 1) > 0)
+					subset.add(functions[ix]);
+			if (test.test(input.me(subset.toArray(new GeneralFunction[0]))))
+				return true;
+		}
+		return false;
 	}
 
 	/**
-	 * Checks if this {@link CommutativeFunction} has a subset (as a new instance of its class) not including one or more of its children satisfying excludeFromSubset that satisfies the condition, including empty and single-element products
+	 * Checks if this {@link CommutativeFunction} has a subset (as a new instance of its class), including empty and single-element functions, that:
+	 * - Is the complement of a subset satisfying {@code excludeFromSubset}
+	 * - Satisfies {@code test}
 	 * @param input the {@link CommutativeFunction} to be searched in
 	 * @param test the condition to be satisfied
-	 * @param excludeFromSubset subset that should be excluded
-	 * @return true if the condition was satisfied by a subset
+	 * @param excludeFromSubset the predicate for the subset that should be excluded
+	 * @return true if the conditions above were satisfied by a subset of {@code input}
 	 */
 	public static boolean existsInOppositeSurfaceSubset(CommutativeFunction input, Predicate<? super GeneralFunction> test, Predicate<? super GeneralFunction> excludeFromSubset) {
 		GeneralFunction[] functions = input.getFunctions();

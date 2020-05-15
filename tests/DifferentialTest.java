6487d97597b75fb0caa1ab42a9a7eb4108bc537a
eiis1000 <erez.m.israeli@gmail.com>
2020-05-15 12:22:37 -0400

    Add integralNull test

--- tests/DifferentialTest.java
+++ tests/DifferentialTest.java
@@ -2,9 +2,9 @@ import functions.GeneralFunction;
 import functions.unitary.transforms.Integral;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static parsing.FunctionParser.*;
+import static org.junit.jupiter.api.Assertions.*;
+import static parsing.FunctionParser.parseInfix;
+import static parsing.FunctionParser.parseSimplified;
 
 public class DifferentialTest {
 	@Test
@@ -29,6 +29,14 @@ public class DifferentialTest {
 	void integral1() {
 		GeneralFunction test = parseInfix("\\int[2x]\\dx");
 		assertTrue(test instanceof Integral);
+	}
+
+	@Test
+	void integralNull() {
+		Integral test = new Integral(parseSimplified("x"));
+		assertThrows(Exception.class, test::execute);
+		assertThrows(Exception.class, test::simplify);
+		assertThrows(Exception.class, () -> test.substituteVariable('x', parseSimplified("y")));
 	}
 
 }

5d5a3b0c995f025ac602ce6df5b76f7602c4d671
eiis1000 <erez.m.israeli@gmail.com>
2020-05-15 09:01:50 -0400

    Document new exceptions

--- src/tools/exceptions/CommandNotFoundException.java
+++ src/tools/exceptions/CommandNotFoundException.java
@@ -1,6 +1,10 @@
 package tools.exceptions;
 
 public class CommandNotFoundException extends IllegalArgumentException {
+	/**
+	 * To be thrown when an invalid command is passed to KeywordInterface
+	 * @param message the message describing the problem
+	 */
 	public CommandNotFoundException(String message) {
 		super(message);
 	}

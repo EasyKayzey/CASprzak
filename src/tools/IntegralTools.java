02bb355a58957c31eafed9d74eb3d086c04bb395
Michael Kasprzak <michaelkasprzak522@gmail.com>
2020-05-20 17:38:33 -0400

    Document IntegralTools

--- src/tools/IntegralTools.java
+++ src/tools/IntegralTools.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.ListIterator;
 
 /**
- * TODO explain
+ * The {@link IntegralTools} class contains miscellaneous methods used in {@link tools.integration}
  */
 public class IntegralTools {
 

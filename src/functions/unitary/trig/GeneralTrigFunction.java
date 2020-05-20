dc96301811e964d7ca87607f5e0cac8dea509308
Michael Kasprzak <michaelkasprzak522@gmail.com>
2020-05-20 19:03:06 -0400

    Document GeneralTrigFunction

--- src/functions/unitary/trig/GeneralTrigFunction.java
+++ src/functions/unitary/trig/GeneralTrigFunction.java
@@ -6,7 +6,7 @@ import functions.Invertible;
 import functions.unitary.UnitaryFunction;
 
 /**
- * TODO explain
+ * The abstract {@link GeneralTrigFunction} class represents amy trigonometric function (normal, hyperbolic, or inverse).
  */
 public abstract class GeneralTrigFunction extends UnitaryFunction implements Invertible, Integrable {
 

c4ecac0ffd938bef863ffc97f72a131669234558
Michael Kasprzak <michaelkasprzak522@gmail.com>
2020-05-22 12:51:45 -0400

    Create the Modulo class

---
+++ src/functions/binary/integer/Modulo.java
@@ -0,0 +1,47 @@
+package functions.binary.integer;
+
+import functions.GeneralFunction;
+import functions.binary.BinaryFunction;
+
+import java.util.Map;
+
+public class Modulo extends BinaryFunction {
+    /**
+     * Constructs a new Modulo
+     * @param function1 The first {@link GeneralFunction} in the binary operation
+     * @param function2 The second {@link GeneralFunction} in the binary operation
+     */
+    public Modulo(GeneralFunction function1, GeneralFunction function2) {
+        super(function1, function2);
+    }
+
+    @Override
+    public BinaryFunction getInstance(GeneralFunction function1, GeneralFunction function2) {
+        return null;
+    }
+
+    @Override
+    public String toString() {
+        return null;
+    }
+
+    @Override
+    public GeneralFunction clone() {
+        return null;
+    }
+
+    @Override
+    public GeneralFunction getDerivative(char varID) {
+        return null;
+    }
+
+    @Override
+    public double evaluate(Map<Character, Double> variableValues) {
+        return 0;
+    }
+
+    @Override
+    public GeneralFunction simplify() {
+        return null;
+    }
+}

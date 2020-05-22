a59aa98fdc69fdd9160dea79a4fb48335f6a1cf5
Michael Kasprzak <michaelkasprzak522@gmail.com>
2020-05-22 12:58:43 -0400

    Implement methods for Modulo

--- src/functions/binary/integer/Modulo.java
+++ src/functions/binary/integer/Modulo.java
@@ -1,7 +1,12 @@
 package functions.binary.integer;
 
+import config.Settings;
 import functions.GeneralFunction;
 import functions.binary.BinaryFunction;
+import functions.special.Constant;
+import tools.DefaultFunctions;
+import tools.ParsingTools;
+import tools.VariableTools;
 
 import java.util.Map;
 
@@ -17,31 +22,42 @@ public class Modulo extends BinaryFunction {
 
     @Override
     public BinaryFunction getInstance(GeneralFunction function1, GeneralFunction function2) {
-        return null;
+        return new Modulo(function1, function2);
     }
 
     @Override
     public String toString() {
-        return null;
+        return "(" + function2.toString() + "%" + function1.toString() + ")";
     }
 
     @Override
     public GeneralFunction clone() {
-        return null;
+        return new Modulo(function1.clone(), function2.clone());
     }
 
     @Override
     public GeneralFunction getDerivative(char varID) {
-        return null;
+        if (VariableTools.doesNotContainsVariable(function1, varID) && VariableTools.doesNotContainsVariable(function2, varID))
+            return DefaultFunctions.ZERO;
+        else
+            throw new UnsupportedOperationException("Modulo has no derivative.");
     }
 
+    @SuppressWarnings("RedundantCast")
     @Override
     public double evaluate(Map<Character, Double> variableValues) {
-        return 0;
+        if (!Settings.enforceIntegerOperations)
+            throw new IllegalStateException("Modulo cannot be used if Settings.enforceIntegerOperations is not enabled.");
+        int argument1 = ParsingTools.toInteger(function1.evaluate(variableValues));
+        int argument2 = ParsingTools.toInteger(function2.evaluate(variableValues));
+        return (double) (argument2 % argument1);
     }
 
+    @SuppressWarnings("RedundantCast")
     @Override
     public GeneralFunction simplify() {
-        return null;
+        if (function1 instanceof Constant constant1 && function2 instanceof Constant constant2)
+            return new Constant((double) (ParsingTools.toInteger(constant2.constant) % ParsingTools.toInteger(constant1.constant)));
+        return new Modulo(function1.simplify(), function2.simplify());
     }
 }

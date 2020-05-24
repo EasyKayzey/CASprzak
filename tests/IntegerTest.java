d828ec107f2fafc5fc5c0e9dceec077170068bef
Michael Kasprzak <michaelkasprzak522@gmail.com>
2020-05-24 00:27:21 -0400

    Create test

--- tests/IntegerTest.java
+++ tests/IntegerTest.java
@@ -1,14 +1,21 @@
 import functions.GeneralFunction;
+import functions.binary.Rand;
 import functions.binary.integer.IntegerQuotient;
 import functions.binary.integer.Modulo;
 import functions.commutative.integer.GCD;
 import functions.commutative.integer.LCM;
 import functions.special.Constant;
+import functions.special.Variable;
+import functions.unitary.piecewise.Ceil;
+import functions.unitary.piecewise.Floor;
+import functions.unitary.piecewise.Round;
+import functions.unitary.trig.normal.Sin;
 import org.junit.jupiter.api.Test;
 
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 @SuppressWarnings("AssertEqualsBetweenInconvertibleTypes")
 public class IntegerTest {
@@ -48,6 +55,31 @@ public class IntegerTest {
         assertEquals(new Constant(3920), new LCM(new Constant(4), new Constant(196), new Constant(80)));
         assertEquals(3920, new LCM(new Constant(4), new Constant(196), new Constant(80)).evaluate(Map.of()));
     }
+
+    @Test
+    void basicFloor() {
+        GeneralFunction test = new Floor(new Sin(new Variable('x')));
+        assertEquals(0, test.evaluate(Map.of('x', 1.0)));
+    }
+
+    @Test
+    void basicCeil() {
+        GeneralFunction test = new Ceil(new Sin(new Variable('x')));
+        assertEquals(1, test.evaluate(Map.of('x', 1.0)));
+    }
+
+    @Test
+    void basicRound() {
+        GeneralFunction test = new Round(new Sin(new Variable('x')));
+        assertEquals(1, test.evaluate(Map.of('x', 1.0)));
+    }
+
+    @Test
+    void basicRand() {
+        GeneralFunction test = new Rand(new Constant(0.0), new Constant(10.0));
+        assertNotEquals(test.evaluate(Map.of()), test.evaluate(Map.of()));
+    }
+
 
 
 }

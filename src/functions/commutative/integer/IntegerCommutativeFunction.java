efdc8a54eae3f49d94d3a418746adcf3323128ce
eiis1000 <erez.m.israeli@gmail.com>
2020-05-23 23:08:46 -0400

    Create IntegerCommutativeFunction

---
+++ src/functions/commutative/integer/IntegerCommutativeFunction.java
@@ -0,0 +1,44 @@
+package functions.commutative.integer;
+
+import config.Settings;
+import functions.GeneralFunction;
+import functions.commutative.CommutativeFunction;
+import tools.DefaultFunctions;
+import tools.ParsingTools;
+import tools.VariableTools;
+
+import java.util.Arrays;
+import java.util.Map;
+
+public abstract class IntegerCommutativeFunction extends CommutativeFunction {
+	/**
+	 * Constructs a new {@link IntegerCommutativeFunction}
+	 * @param functions the {@link GeneralFunction}s that will be acted on
+	 */
+	public IntegerCommutativeFunction(GeneralFunction... functions) {
+		super(functions);
+	}
+
+	@Override
+	public GeneralFunction getDerivative(char varID) {
+		if (VariableTools.doesNotContainsVariable(this, varID))
+			return DefaultFunctions.ZERO;
+		else
+			throw new UnsupportedOperationException("IntegerCommutativeFunctions have no derivative.");
+	}
+
+	@Override
+	public double evaluate(Map<Character, Double> variableValues) {
+		if (!Settings.enforceIntegerOperations)
+			throw new IllegalStateException("IntegerQuotient cannot be used if Settings.enforceIntegerOperations is not enabled.");
+		int[] toOperate = Arrays.stream(functions).mapToInt(f -> ParsingTools.toInteger(f.evaluate(variableValues))).toArray();
+		return operate(toOperate);
+	}
+
+	/**
+	 * Performs the operation of this function on the inputs
+	 * @param operands the list of inputs
+	 * @return the operation applied to the inputs
+	 */
+	protected abstract int operate(int... operands);
+}

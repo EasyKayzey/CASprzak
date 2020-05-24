66d3fd8e4e65040e64690963dbfa876bc69f769e
Michael Kasprzak <michaelkasprzak522@gmail.com>
2020-05-23 23:08:27 -0400

    Implement Ceil class

---
+++ src/functions/unitary/piecewise/Ceil.java
@@ -0,0 +1,32 @@
+package functions.unitary.piecewise;
+
+import functions.GeneralFunction;
+import functions.unitary.UnitaryFunction;
+import tools.DefaultFunctions;
+
+import java.util.Map;
+
+public class Ceil extends PiecewiseFunction{
+    /**
+     * Constructs a new {@link Ceil)
+     * @param operand The function which ceiling is operating on
+     */
+    public Ceil(GeneralFunction operand) {
+        super(operand);
+    }
+
+    @Override
+    public UnitaryFunction getInstance(GeneralFunction operand) {
+        return new Ceil(operand);
+    }
+
+    @Override
+    public GeneralFunction getDerivative(char varID) {
+        return DefaultFunctions.ZERO;
+    }
+
+    @Override
+    public double evaluate(Map<Character, Double> variableValues) {
+        return Math.ceil(operand.evaluate(variableValues));
+    }
+}

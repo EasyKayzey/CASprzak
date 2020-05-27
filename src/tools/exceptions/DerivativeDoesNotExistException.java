6978038d25b6fb4fbca775ca6d4b733d0106834a
eiis1000 <erez.m.israeli@gmail.com>
2020-05-27 15:58:42 -0400

    Change DerivativeDoesNotExistException signature

--- src/tools/exceptions/DerivativeDoesNotExistException.java
+++ src/tools/exceptions/DerivativeDoesNotExistException.java
@@ -1,13 +1,27 @@
 package tools.exceptions;
 
+import functions.GeneralFunction;
+
 import javax.naming.OperationNotSupportedException;
 
 public class DerivativeDoesNotExistException extends OperationNotSupportedException {
+
+    private final GeneralFunction function;
+
     /**
      * To be thrown when a derivative of a function does not exist
-     * @param message the message describing the problem
+     * @param function the non-differentiable function
      */
-    public DerivativeDoesNotExistException(String message) {
-        super(message);
+    public DerivativeDoesNotExistException(GeneralFunction function) {
+        super("The derivative of " + function.getClass().getSimpleName() + " does not exist.");
+        this.function = function;
+    }
+
+    /**
+     * Return the non-differentiable function
+     * @return the non-differentiable function
+     */
+    public GeneralFunction getFunction() {
+        return function;
     }
 }

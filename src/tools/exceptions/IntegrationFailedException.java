77e05c17b1626b8efdd0dfc81a1f45dd0e38595a
eiis1000 <erez.m.israeli@gmail.com>
2020-05-27 16:03:04 -0400

    Change IntegrationFailedException signature

--- src/tools/exceptions/IntegrationFailedException.java
+++ src/tools/exceptions/IntegrationFailedException.java
@@ -1,11 +1,25 @@
 package tools.exceptions;
 
+import functions.GeneralFunction;
+
 public class IntegrationFailedException extends TransformFailedException {
+
+	private final GeneralFunction function;
+
 	/**
-	 * To be thrown when integration has failed
-	 * @param message more information about the failed integration
+	 * To be thrown when the integral of a function cannot be found
+	 * @param function the non-integrable function
 	 */
-	public IntegrationFailedException(String message) {
-		super(message);
+	public IntegrationFailedException(GeneralFunction function) {
+		super("The integral of " + function.getClass().getSimpleName() + " could not be found.");
+		this.function = function;
+	}
+
+	/**
+	 * Returns the non-integrable function
+	 * @return the non-integrable function
+	 */
+	public GeneralFunction getFunction() {
+		return function;
 	}
 }

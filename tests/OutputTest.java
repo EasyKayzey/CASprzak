7cae743fa16222e6a21536e70b15f7949171bc09
eiis1000 <erez.m.israeli@gmail.com>
2020-06-13 16:49:58 -0400

    Create endpointTest1

--- tests/OutputTest.java
+++ tests/OutputTest.java
@@ -1,2 +1,14 @@
+import functions.special.Constant;
+import org.junit.jupiter.api.Test;
+import output.OutputEndpoint;
+import tools.DefaultFunctions;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class OutputTest {
+
+	@Test
+	void endpointTest1() {
+		assertEquals(DefaultFunctions.TWO.toOutputFunction(), new OutputEndpoint(new Constant(2)));
+	}
 }

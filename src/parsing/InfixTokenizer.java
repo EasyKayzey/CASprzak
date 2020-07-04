e4619401305dfeac0c3433feb15932561361a795
eiis1000 <erez.m.israeli@gmail.com>
2020-07-04 00:11:09 -0400

    Add escaped variables to \d

--- src/parsing/InfixTokenizer.java
+++ src/parsing/InfixTokenizer.java
@@ -57,7 +57,7 @@ public class InfixTokenizer {
 			"(?=[a-zA-Z\\\\(\\d[^\\x00-\\x7F]])" 					// Followed by a digit, alphabetic char, open-parenthesis, or non-ascii character
 	);
 	private static final Pattern differential = Pattern.compile(
-			"\\\\d\\s?(?=[a-zA-Z[^\\x00-\\x7F]](?![a-zA-Z[^\\x00-\\x7F]]))"
+			"\\\\d\\s?(?=[a-zA-Z[^\\x00-\\x7F]](?![a-zA-Z[^\\x00-\\x7F]])|\\\\)"
 	);
 	private static final Pattern partialDerivative = Pattern.compile("d/d");
 	private static final Pattern endPD = Pattern.compile(

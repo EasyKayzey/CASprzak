f926023db29507b6dc984e641400fcbb90410ede
Michael Kasprzak <michaelkasprzak522@gmail.com>
2020-07-06 23:41:12 -0400

    Add OutputTrigFunction to TrigFunction

--- src/functions/unitary/trig/normal/TrigFunction.java
+++ src/functions/unitary/trig/normal/TrigFunction.java
@@ -2,6 +2,8 @@ package functions.unitary.trig.normal;
 
 import functions.GeneralFunction;
 import functions.unitary.trig.GeneralTrigFunction;
+import output.OutputFunction;
+import output.OutputUnitary;
 
 /**
  * The abstract {@link TrigFunction} class represents any non-inverse trigonometric function (circular or hyperbolic).
@@ -14,5 +16,23 @@ public abstract class TrigFunction extends GeneralTrigFunction {
 	 */
 	public TrigFunction(GeneralFunction operand) {
 		super(operand);
+	}
+
+	public OutputFunction toOutputFunction() {
+		return new OutputTrigFunction(operand.toOutputFunction(), this.getClass().getSimpleName().toLowerCase());
+	}
+
+	private static class OutputTrigFunction extends OutputUnitary {
+
+		public OutputTrigFunction(OutputFunction operand, String functionString) {
+			super(functionString, operand);
+		}
+
+		@Override
+		public String toLatex() {
+			return "\\" + functionName + " \\left( " + operand.toLatex() + " \\right)";
+		}
+
+
 	}
 }

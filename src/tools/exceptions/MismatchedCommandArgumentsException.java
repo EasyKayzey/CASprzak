105d314c4794d1a1cb84b21b9cd1d09dbd1ad1ca
Michael Kasprzak <michaelkasprzak522@gmail.com>
2020-07-13 14:02:13 -0400

    Create MismatchedCommandArgumentsException

---
+++ src/tools/exceptions/MismatchedCommandArgumentsException.java
@@ -0,0 +1,12 @@
+package tools.exceptions;
+
+public class MismatchedCommandArgumentsException extends IllegalArgumentException{
+    /**
+     * To be thrown when an input is not the required length in KeywordInterface
+     * @param expectedLength the expected length of the input
+     * @param actualLength the actual length of the input
+     */
+    public MismatchedCommandArgumentsException(int expectedLength, int actualLength) {
+        super("Expected " + expectedLength +  " arguments and received " + actualLength + ".");
+    }
+}

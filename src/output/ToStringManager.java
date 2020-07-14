54a34e16589061ec959d8321d87b7be26028a454
eiis1000 <erez.m.israeli@gmail.com>
2020-07-14 00:30:22 -0400

    Implement maybeParenthesize

--- src/output/ToStringManager.java
+++ src/output/ToStringManager.java
@@ -1,4 +1,15 @@
 package output;
 
 public class ToStringManager {
+	/**
+	 * Adds parentheses to `function` only if it is an {@link OutputCommutative} or {@link OutputBinary}
+	 * @param function the function to be maybe parenthesized
+	 * @return the function, possibly parenthesized
+	 */
+	public static OutputFunction maybeParenthesize(OutputFunction function) {
+		if (function instanceof OutputCommutative || function instanceof OutputBinary)
+			return new OutputParenthesizer(function);
+		else
+			return function;
+	}
 }

b110b17e3c5856894bbc43c225bafcdb7c2ad6db
eiis1000 <erez.m.israeli@gmail.com>
2020-07-14 15:29:32 -0400

    Document OutputString

--- src/output/OutputString.java
+++ src/output/OutputString.java
@@ -3,6 +3,9 @@ package output;
 import java.util.Collection;
 import java.util.Collections;
 
+/**
+ * Provides an {@link OutputFunction} whose representations are a simple string
+ */
 public class OutputString implements OutputFunction {
 
 	private final String string;

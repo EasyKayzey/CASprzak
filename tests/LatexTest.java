959ba89c693628343a794172fe6c9b8b7740fc7f
eiis1000 <erez.m.israeli@gmail.com>
2020-07-19 23:46:32 -0400

    Add adjacentWords tests

--- tests/LatexTest.java
+++ tests/LatexTest.java
@@ -61,4 +61,16 @@ public class LatexTest {
 		GeneralFunction test = FunctionParser.parseInfix("1 + \\sin(x)");
 		assertEquals(1, test.evaluate(Map.of("x", 3.0)), .3);
 	}
+
+	@Test
+	void adjacentWords1() {
+		GeneralFunction test = FunctionParser.parseInfix("\\pi\\Gamma");
+		assertEquals(6.3, test.evaluate(Map.of("Γ", 2.)), .3);
+	}
+
+	@Test
+	void adjacentWords2() {
+		GeneralFunction test = FunctionParser.parseInfix("\\epsilon\\Gamma\\epsilon\\pi");
+		assertEquals(6.3, test.evaluate(Map.of("ϵ", 1., "Γ", 2.)), .3);
+	}
 }

33342b226051f2fd059d5f1d3f695e36a92a75e5
eiis1000 <erez.m.israeli@gmail.com>
2020-08-02 20:12:17 -0400

    Revert "Make no-argument evaluation method use collections empty map"
    This reverts commit db40dcd5

--- src/functions/GeneralFunction.java
+++ src/functions/GeneralFunction.java
@@ -18,7 +18,6 @@ import functions.unitary.trig.normal.TrigFunction;
 import org.jetbrains.annotations.NotNull;
 import tools.MiscTools;
 
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
@@ -75,7 +74,7 @@ public abstract class GeneralFunction implements Evaluable, Differentiable, Simp
 	 * @return the function evaluated with no arguments
 	 */
 	public double evaluate() {
-		return evaluate(Collections.emptyMap());
+		return evaluate(Map.of());
 	}
 
 

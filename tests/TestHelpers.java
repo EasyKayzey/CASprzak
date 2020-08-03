3cfc34e08017b30feca5c87c371219126df14080
eiis1000 <erez.m.israeli@gmail.com>
2020-08-03 00:45:10 -0400

    Expand TestHelpers

--- tests/TestHelpers.java
+++ tests/TestHelpers.java
@@ -2,6 +2,9 @@ import functions.GeneralFunction;
 import org.junit.jupiter.params.converter.ArgumentConversionException;
 import org.junit.jupiter.params.converter.SimpleArgumentConverter;
 import parsing.FunctionParser;
+import tools.ParsingTools;
+
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -13,6 +16,25 @@ public class TestHelpers {
 		protected Object convert(Object object, Class<?> targetType) throws ArgumentConversionException {
 			assertEquals(GeneralFunction.class, targetType);
 			return FunctionParser.parseInfix((String) object);
+		}
+	}
+
+	public static class XMapper extends SimpleArgumentConverter {
+
+		@Override
+		protected Object convert(Object object, Class<?> targetType) throws ArgumentConversionException {
+			assertEquals(Map.class, targetType);
+			return Map.of("x", ParsingTools.getConstant((String) object));
+		}
+	}
+
+	public static class XYMapper extends SimpleArgumentConverter {
+
+		@Override
+		protected Object convert(Object object, Class<?> targetType) throws ArgumentConversionException {
+			assertEquals(Map.class, targetType);
+			String[] cons = ((String) object).split(",");
+			return Map.of("x", ParsingTools.getConstant(cons[0]), "y", ParsingTools.getConstant(cons[1]));
 		}
 	}
 

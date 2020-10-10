64563033e3b8129a5d03be2438d33f78d5d64b1e
eiis1000 <erez.m.israeli@gmail.com>
2020-10-09 20:14:08 -0400

    Begin defaults assignment and documentation of DefaultSimplificationSettings user settings

--- core/src/java/show/ezkz/casprzak/core/tools/defaults/DefaultSimplificationSettings.java
+++ core/src/java/show/ezkz/casprzak/core/tools/defaults/DefaultSimplificationSettings.java
@@ -1,24 +1,91 @@
 package show.ezkz.casprzak.core.tools.defaults;
 
 import show.ezkz.casprzak.core.config.SimplificationSettings;
+import show.ezkz.casprzak.core.functions.unitary.transforms.Transformation;
 
 public class DefaultSimplificationSettings {
 	// The settings below are the settings for `user`
-	public static boolean simplifyFunctionsOfConstants;
-	public static boolean simplifyFunctionsOfSpecialConstants;
-	public static boolean distributeExponentsOverMultiplication;
-	public static boolean addExponentsInProducts;
-	public static boolean distributeMultiplicationOverAddition;
-	public static boolean multiplyExponentsOfExponents;
-	public static boolean executeTransformsOnSimplify;
-	public static boolean enforceDomainAndRange;
-	public static boolean simplifyLogAddition;
-	public static boolean expandLogOfProducts;
-	public static boolean extractLogExponents;
-	public static boolean insertLogExponents;
-	public static boolean doChangeOfBase;
-	public static boolean doTrigIdentities;
-	public static boolean trigComposition;
+
+	/**
+	 * Denotes whether functions of constants should be simplified. Ex: {@code \sin(\pi/2) -> 1}
+	 */
+	public static boolean simplifyFunctionsOfConstants = true;
+
+	/**
+	 * Denotes whether or not special {@code Constant}s get simplified into decimal under addition, multiplication, or exponentiation.
+	 */
+	public static boolean simplifyFunctionsOfSpecialConstants = false;
+
+	/**
+	 * Denotes whether exponents should be distributed over multiplication in {@code simplify(settings)}. Ex: {@code (2x)^2 -> 4x^2}
+	 */
+	public static boolean distributeExponentsOverMultiplication = true;
+
+	/**
+	 *
+	 */
+	public static boolean addExponentsInProducts = true;
+
+	/**
+	 * Denotes whether products are distributed over addition in {@code simplify(settings)}. Ex: {@code x(y+z) -> (xy+xz)}
+	 */
+	public static boolean distributeMultiplicationOverAddition = true;
+
+	/**
+	 *
+	 */
+	public static boolean multiplyExponentsOfExponents = true;
+
+	/**
+	 * Denotes whether a {@link Transformation} should execute its action when {@code simplify(settings)} is called.
+	 */
+	public static boolean executeTransformsOnSimplify = true;
+
+	/**
+	 * Denotes whether or not inverse simplifications conserve domain and range
+	 */
+	public static boolean enforceDomainAndRange = true;
+
+	/**
+	 *
+	 */
+	public static boolean simplifyLogAddition = true;
+
+	/**
+	 *
+	 */
+	public static boolean expandLogOfProducts = false;
+
+	/**
+	 *
+	 */
+	public static boolean extractLogExponents = true;
+
+	/**
+	 *
+	 */
+	public static boolean insertLogExponents = false;
+
+	/**
+	 *
+	 */
+	public static boolean doChangeOfBase = false;
+
+	/**
+	 *
+	 */
+	public static boolean doTrigIdentities = true;
+
+	/**
+	 *
+	 */
+	public static boolean trigComposition = false;
+
+	/**
+	 *
+	 */
+	public static boolean simplifyInverses = true;
+
 
 	public static SimplificationSettings user;
 	public static final SimplificationSettings minimal = new SimplificationSettings(
@@ -30,6 +97,7 @@ public class DefaultSimplificationSettings {
 			false,
 			false,
 			true,
+			false,
 			false,
 			false,
 			false,
@@ -53,7 +121,8 @@ public class DefaultSimplificationSettings {
 			false,
 			false,
 			false,
-			false
+			false,
+			true
 	);
 	public static final SimplificationSettings aggressive = new SimplificationSettings(
 			true,
@@ -69,6 +138,7 @@ public class DefaultSimplificationSettings {
 			true,
 			false,
 			false,
+			true,
 			true,
 			true
 	);
@@ -93,7 +163,8 @@ public class DefaultSimplificationSettings {
 				insertLogExponents,
 				doChangeOfBase,
 				doTrigIdentities,
-				trigComposition
+				trigComposition,
+				simplifyInverses
 		);
 	}
 

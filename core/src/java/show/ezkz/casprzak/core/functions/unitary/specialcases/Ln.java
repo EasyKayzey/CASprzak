c72a599f26a5879866e79fc37d1c42f01dd661f8
eiis1000 <erez.m.israeli@gmail.com>
2020-10-14 23:11:35 -0400

    Add special check for ln(e)

--- core/src/java/show/ezkz/casprzak/core/functions/unitary/specialcases/Ln.java
+++ core/src/java/show/ezkz/casprzak/core/functions/unitary/specialcases/Ln.java
@@ -12,6 +12,9 @@ import show.ezkz.casprzak.core.tools.helperclasses.LogInterface;
 
 import java.util.Map;
 
+import static show.ezkz.casprzak.core.tools.defaults.DefaultFunctions.E;
+import static show.ezkz.casprzak.core.tools.defaults.DefaultFunctions.ONE;
+
 
 public class Ln extends SpecialCaseBinaryFunction implements LogInterface {
 	/**
@@ -37,13 +40,21 @@ public class Ln extends SpecialCaseBinaryFunction implements LogInterface {
 	}
 
 	public BinaryFunction getClassForm() {
-		return new Logb(operand, DefaultFunctions.E);
+		return new Logb(operand, E);
 	}
 
 	public Class<?> getInverse() {
 		return Exp.class;
 	}
 
+
+	@Override
+	public GeneralFunction simplify(SimplificationSettings settings) {
+		GeneralFunction current = super.simplify(settings);
+		if ((!settings.simplifyFunctionsOfConstants || !settings.simplifyFunctionsOfSpecialConstants) && current instanceof Ln ln && ln.operand.equals(E))
+			return ONE;
+		return current;
+	}
 
 	@Override
 	public GeneralFunction simplifyInverse(SimplificationSettings settings) {
@@ -65,6 +76,6 @@ public class Ln extends SpecialCaseBinaryFunction implements LogInterface {
 
 	@Override
 	public GeneralFunction base() {
-		return DefaultFunctions.E;
+		return E;
 	}
 }

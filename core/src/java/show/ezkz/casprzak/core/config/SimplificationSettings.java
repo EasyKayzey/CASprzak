0e0870aa784460b485de550c9093af3bcedbfe5f
eiis1000 <erez.m.israeli@gmail.com>
2020-10-14 23:45:46 -0400

    Move documentation of settings from DefaultSimplificationSettings to SimplificationSettings

--- core/src/java/show/ezkz/casprzak/core/config/SimplificationSettings.java
+++ core/src/java/show/ezkz/casprzak/core/config/SimplificationSettings.java
@@ -1,5 +1,6 @@
 package show.ezkz.casprzak.core.config;
 
+import show.ezkz.casprzak.core.functions.commutative.Product;
 import show.ezkz.casprzak.core.tools.exceptions.IncompatibleSettingsException;
 
 /**
@@ -8,20 +9,80 @@ import show.ezkz.casprzak.core.tools.exceptions.IncompatibleSettingsException;
  * {@link show.ezkz.casprzak.core.tools.defaults.DefaultSimplificationSettings} contains often-used examples of simplification parameters.
  */
 public class SimplificationSettings {
+
+	/**
+	 * Denotes whether functions of constants should be simplified. Ex: {@code 2*3^2 -> 18}
+	 */
 	public final boolean simplifyFunctionsOfConstants;
+
+	/**
+	 * Denotes whether or not special {@code Constant}s get simplified into decimal under addition, multiplication, or exponentiation. Ex: {@code \pi^2 -> 9.87}
+	 */
 	public final boolean simplifyFunctionsOfSpecialConstants;
+
+	/**
+	 * Denotes whether exponents should be distributed over multiplication in {@code simplify(settings)}. Ex: {@code (2x)^2 -> 4x^2}
+	 */
 	public final boolean distributeExponentsOverMultiplication;
+
+	/**
+	 * Denotes whether like terms in a product should have their exponents added. Ex: {@code x*x^2 -> x^3}
+	 */
 	public final boolean addExponentsInProducts;
+
+	/**
+	 * Denotes whether products are distributed over addition in {@code simplify(settings)}. Ex: {@code x(y+z) -> (xy+xz)}
+	 */
 	public final boolean distributeMultiplicationOverAddition;
+
+	/**
+	 * Denotes whether nested powers should have their exponents multiplied. Ex: {@code (x^3)^2 -> x^6}
+	 */
 	public final boolean multiplyExponentsOfExponents;
+
+	/**
+	 * Denotes whether a {@link show.ezkz.casprzak.core.functions.unitary.transforms.Transformation} should execute its action when {@code simplify(settings)} is called. Ex: {@code d/dx[x^2] -> 2x} (without explicit execution)
+	 */
 	public final boolean executeTransformsOnSimplify;
+
+	/**
+	 * Denotes whether or not inverse simplifications conserve domain and range. Ex: {@code asin(sin(7*\pi/2)) -> -\pi/2} (without evaluation)
+	 */
 	public final boolean enforceDomainAndRange;
+
+	/**
+	 * Denotes whether sums of logs should become products of their arguments. Ex: {@code ln(x) + ln(y) -> ln(x*y)}
+	 */
 	public final boolean simplifyLogAddition;
+
+	/**
+	 * Denotes whether products inside logs should become sums of logs. Ex: {@code ln(x*y) -> ln(x) + ln(y)}
+	 */
 	public final boolean expandLogOfProducts;
+
+	/**
+	 * Denotes whether exponents inside of logs are pulled out. Ex. {@code ln(x^x) = xln(x)}
+	 */
 	public final boolean extractLogExponents;
+
+	/**
+	 * Denotes whether or not {@link show.ezkz.casprzak.core.tools.algebra.LogSimplify#logChainRule(SimplificationSettings, Product)} is performed. Ex: {@code logb_a(b) * logb_b(c) * logb_x(y) / logb_x(z) = logb_a(c) * logb_z(y)}
+	 */
 	public final boolean doChangeOfBase;
+
+	/**
+	 * Denotes whether trig identities are performed. Ex. {@code sin^2(x) + cos^2(x) = 1 }
+	 */
 	public final boolean doTrigIdentities;
+
+	/**
+	 * Denotes whether trig composition simplify to algebraic expressions. Ex. {@code sin(arccos(x)) = \sqrt(1-x^2)}
+	 */
 	public final boolean trigComposition;
+
+	/**
+	 * Denotes whether inverse should cancel. Ex {@code ln(e^x)=x}
+	 */
 	public final boolean simplifyInverses;
 
 	public SimplificationSettings(boolean simplifyFunctionsOfConstants,
--- core/src/java/show/ezkz/casprzak/core/tools/defaults/DefaultSimplificationSettings.java
+++ core/src/java/show/ezkz/casprzak/core/tools/defaults/DefaultSimplificationSettings.java
@@ -6,87 +6,29 @@ import show.ezkz.casprzak.core.functions.commutative.Product;
 import show.ezkz.casprzak.core.functions.unitary.transforms.Transformation;
 
 /**
- * The {@link DefaultSimplificationSettings} class contains the simplification settings for minimal, automatic, and aggressive simplification.
+ * The {@link DefaultSimplificationSettings} class contains the {@link SimplificationSettings} for minimal, automatic, and aggressive simplification.
  * It also contains the user simplification settings (as read by {@link Settings#parseConfig()} and modified at any time) as public static variables.
  * The {@link #user} settings are regenerated each time {@link #regenerateUser()} is called, which should be done whenever the static settings are changed.
+ * The documentation for each setting can be found in the {@link SimplificationSettings} class.
  */
 public class DefaultSimplificationSettings {
 
-	// The settings below are the settings for `user`
+	// The settings below are the settings for `user`, documented in the `SimplificationSettings` class
 
-	/**
-	 * Denotes whether functions of constants should be simplified. Ex: {@code 2*3^2 -> 18}
-	 */
 	public static boolean simplifyFunctionsOfConstants = true;
-
-	/**
-	 * Denotes whether or not special {@code Constant}s get simplified into decimal under addition, multiplication, or exponentiation. Ex: {@code \pi^2 -> 9.87}
-	 */
 	public static boolean simplifyFunctionsOfSpecialConstants = false;
-
-	/**
-	 * Denotes whether exponents should be distributed over multiplication in {@code simplify(settings)}. Ex: {@code (2x)^2 -> 4x^2}
-	 */
 	public static boolean distributeExponentsOverMultiplication = true;
-
-	/**
-	 * Denotes whether like terms in a product should have their exponents added. Ex: {@code x*x^2 -> x^3}
-	 */
 	public static boolean addExponentsInProducts = true;
-
-	/**
-	 * Denotes whether products are distributed over addition in {@code simplify(settings)}. Ex: {@code x(y+z) -> (xy+xz)}
-	 */
 	public static boolean distributeMultiplicationOverAddition = true;
-
-	/**
-	 * Denotes whether nested powers should have their exponents multiplied. Ex: {@code (x^3)^2 -> x^6}
-	 */
 	public static boolean multiplyExponentsOfExponents = true;
-
-	/**
-	 * Denotes whether a {@link Transformation} should execute its action when {@code simplify(settings)} is called. Ex: {@code d/dx[x^2] -> 2x} (without explicit execution)
-	 */
 	public static boolean executeTransformsOnSimplify = true;
-
-	/**
-	 * Denotes whether or not inverse simplifications conserve domain and range. Ex: {@code asin(sin(7*\pi/2)) -> -\pi/2} (without evaluation)
-	 */
 	public static boolean enforceDomainAndRange = true;
-
-	/**
-	 * Denotes whether sums of logs should become products of their arguments. Ex: {@code ln(x) + ln(y) -> ln(x*y)}
-	 */
 	public static boolean simplifyLogAddition = true;
-
-	/**
-	 * Denotes whether products inside logs should become sums of logs. Ex: {@code ln(x*y) -> ln(x) + ln(y)}
-	 */
 	public static boolean expandLogOfProducts = false;
-
-	/**
-	 * Denotes whether exponents inside of logs are pulled out. Ex. {@code ln(x^x) = xln(x)}
-	 */
 	public static boolean extractLogExponents = true;
-
-	/**
-	 * Denotes whether or not {@link show.ezkz.casprzak.core.tools.algebra.LogSimplify#logChainRule(SimplificationSettings, Product)} is performed. Ex. Ex: {@code logb_a(b) * logb_b(c) * logb_x(y) / logb_x(z) = logb_a(c) * logb_z(y)}
-	 */
 	public static boolean doChangeOfBase = false;
-
-	/**
-	 * Denotes whether trig identities are performed. Ex. {@code sin^2(x) + cos^2(x) = 1 }
-	 */
 	public static boolean doTrigIdentities = true;
-
-	/**
-	 * Denotes whether trig composition simplify to algebraic expressions. Ex. {@code sin(arccos(x)) = \sqrt(1-x^2)}
-	 */
 	public static boolean trigComposition = false;
-
-	/**
-	 * Denotes whether inverse should cancel. Ex {@code ln(e^x)=x}
-	 */
 	public static boolean simplifyInverses = true;
 
 

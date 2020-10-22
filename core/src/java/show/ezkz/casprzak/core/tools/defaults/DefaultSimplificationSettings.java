ac933fbfd5d27d86e2549098d50f784537b5a82c
eiis1000 <erez.m.israeli@gmail.com>
2020-10-22 17:01:34 -0400

    Fix whitespace

--- core/src/java/show/ezkz/casprzak/core/tools/defaults/DefaultSimplificationSettings.java
+++ core/src/java/show/ezkz/casprzak/core/tools/defaults/DefaultSimplificationSettings.java
@@ -107,7 +107,7 @@ public class DefaultSimplificationSettings {
 			true
 	);
 
-	private DefaultSimplificationSettings() {}
+	private DefaultSimplificationSettings(){}
 
 	/**
 	 * Regenerates {@link #user} settings from stored static settings

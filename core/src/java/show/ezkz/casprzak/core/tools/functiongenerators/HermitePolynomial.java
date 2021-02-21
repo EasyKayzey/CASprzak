89d39dcac18ac3ddc52e64f846a018191a1e949b
Michael Kasprzak <michaelkasprzak522@gmail.com>
2021-02-21 01:20:55 -0500

    Implement HermitePolynomial class

---
+++ core/src/java/show/ezkz/casprzak/core/tools/functiongenerators/HermitePolynomial.java
@@ -0,0 +1,93 @@
+package show.ezkz.casprzak.core.tools.functiongenerators;
+
+import show.ezkz.casprzak.core.config.Settings;
+import show.ezkz.casprzak.core.functions.GeneralFunction;
+import show.ezkz.casprzak.core.functions.commutative.Product;
+import show.ezkz.casprzak.core.functions.endpoint.Constant;
+import show.ezkz.casprzak.core.functions.endpoint.Variable;
+import show.ezkz.casprzak.core.functions.unitary.specialcases.Exp;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static java.lang.Math.pow;
+import static show.ezkz.casprzak.core.tools.MiscTools.factorial;
+import static show.ezkz.casprzak.core.tools.defaults.DefaultFunctions.*;
+
+/**
+ * The methods in {@link HermitePolynomial} deal with <a href="https://en.wikipedia.org/wiki/Hermite_polynomials#Hermite_functions">Hermite Polynomials</a>.
+ */
+public class HermitePolynomial {
+
+	private static final Map<Integer, GeneralFunction> cache = new HashMap<>();
+
+	/**
+	 * Returns the nth Hermite polynomial with the default variable defined with {@link Settings#singleVariableDefault}
+	 * @param n the nth polynomial
+	 * @return nth Hermite polynomial
+	 */
+	public static GeneralFunction hermitePolynomial(int n) {
+		if (Settings.cacheDerivatives && cache.containsKey(n))
+			return cache.get(n);
+
+		GeneralFunction polynomial = new Product(
+				new Constant(pow(-1, n)),
+				new Exp(square(new Variable(Settings.singleVariableDefault))),
+				new Exp(negative(square(new Variable(Settings.singleVariableDefault))))
+						.simplify()
+						.getNthDerivative(Settings.singleVariableDefault, n)).simplify();
+		cache.put(n, polynomial);
+		return polynomial;
+	}
+
+	/**
+	 * Returns the nth Hermite polynomial with a variable of the specified String
+	 * @param n the nth polynomial
+	 * @param variable the String of the {@link Variable} of the polynomial
+	 * @return nth Hermite polynomial
+	 */
+	public static GeneralFunction hermitePolynomial(int n, String variable) {
+		GeneralFunction polynomial = hermitePolynomial(n);
+		if (Settings.singleVariableDefault.equals(variable))
+			return polynomial;
+		else {
+			Map<String, Variable> substitution = new HashMap<>() {
+				{
+					put(Settings.singleVariableDefault, new Variable(variable));
+				}
+			};
+			return polynomial.substituteVariables(substitution);
+		}
+	}
+
+	/**
+	 * Returns the nth normalized Hermite polynomial with the default variable defined with {@link Settings#singleVariableDefault}
+	 * @param n the nth polynomial
+	 * @return nth normalized Hermite polynomial
+	 */
+	public static GeneralFunction normalHermitePolynomial(int n) {
+		return new Product(reciprocal(normalizingConstant(n)), hermitePolynomial(n)).simplify();
+	}
+
+	/**
+	 * Returns the nth normalized Hermite polynomial with a variable of the specified String
+	 * @param n the nth polynomial
+	 * @param variable the String of the {@link Variable} of the polynomial
+	 * @return nth normalized Hermite polynomial
+	 */
+	public static GeneralFunction normalHermitePolynomial(int n, String variable) {
+		return new Product(reciprocal(normalizingConstant(n)), hermitePolynomial(n, variable)).simplify();
+	}
+
+	/**
+	 * The normalizing constant of the nth Hermite polynomial
+	 * ie If you wanted to normalize, you would do {@code 1/N * L_n}
+	 * @param n the nth polynomial
+	 * @return normalizing constant of the nth Hermite polynomial
+	 */
+	public static GeneralFunction normalizingConstant(int n) {
+		return sqrt(new Product(new Constant(pow(2,n)*factorial(n)), sqrt(PI))).simplify();
+	}
+
+
+}

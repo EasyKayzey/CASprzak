51a295329860264a3fbd2800cee5195dff0e7725
Michael Kasprzak <michaelkasprzak522@gmail.com>
2021-02-21 02:57:48 -0500

    Update TaylorSeries to be able to use new variables

--- core/src/java/show/ezkz/casprzak/core/tools/functiongenerators/TaylorSeries.java
+++ core/src/java/show/ezkz/casprzak/core/tools/functiongenerators/TaylorSeries.java
@@ -10,6 +10,7 @@ import show.ezkz.casprzak.core.functions.endpoint.Variable;
 import show.ezkz.casprzak.core.tools.MiscTools;
 import show.ezkz.casprzak.core.tools.VariableTools;
 
+import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -28,6 +29,49 @@ public class TaylorSeries {
     public static GeneralFunction makeTaylorSeries(GeneralFunction function, int degree) {
         return makeTaylorSeries(function, degree, 0);
     }
+
+	/**
+	 * Returns a Maclaurin series of the specified function of a given degree
+	 * @param function the function whose Maclaurin series is being found
+	 * @param degree the degree of the Maclaurin polynomial
+	 * @param variable the String of the {@link Variable} of the polynomial
+	 * @return a Maclaurin series of the specified function
+	 */
+	public static GeneralFunction makeTaylorSeries(GeneralFunction function, int degree, String variable) {
+		GeneralFunction taylorSeries = makeTaylorSeries(function, degree);
+		if (Settings.singleVariableDefault.equals(variable))
+			return taylorSeries;
+		else {
+			Map<String, Variable> substitution = new HashMap<>() {
+				{
+					put(Settings.singleVariableDefault, new Variable(variable));
+				}
+			};
+			return taylorSeries.substituteVariables(substitution);
+		}
+	}
+
+	/**
+	 * Returns a Taylor series of the specified function at the specified center of a given degree
+	 * @param function the function whose Taylor series is being found
+	 * @param degree the degree of the Taylor polynomial
+	 * @param center where the Taylor series is centered
+	 * @param variable the String of the {@link Variable} of the polynomial
+	 * @return a Taylor series of the specified function
+	 */
+	public static GeneralFunction makeTaylorSeries(GeneralFunction function, int degree, double center, String variable) {
+		GeneralFunction taylorSeries = makeTaylorSeries(function, degree, center);
+		if (Settings.singleVariableDefault.equals(variable))
+			return taylorSeries;
+		else {
+			Map<String, Variable> substitution = new HashMap<>() {
+				{
+					put(Settings.singleVariableDefault, new Variable(variable));
+				}
+			};
+			return taylorSeries.substituteVariables(substitution);
+		}
+	}
 
     /**
      * Returns a Taylor series of the specified function at the specified center of a given degree
